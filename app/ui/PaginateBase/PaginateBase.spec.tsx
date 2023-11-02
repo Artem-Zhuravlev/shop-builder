ecb6cdd17c2d579ee3c1234d0179591b1b93a1b5
Artem <mstrjesm@gmail.com>
2023-11-02 23:47:48 +0200

    feat(paginate-base): add test

---
+++ app/ui/PaginateBase/PaginateBase.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { PaginateBase } from './PaginateBase';
+
+describe('PaginateBase component', () => {
+  test('calls onPageChange when a page is clicked', () => {
+    const onPageChangeMock = jest.fn();
+    const pageCount = 10;
+
+    const { getAllByRole } = render(
+      <PaginateBase
+        onPageChange={onPageChangeMock}
+        pageCount={pageCount}
+      />
+    );
+
+    const pages = getAllByRole('button', { name: /Page \d+/ });
+    fireEvent.click(pages[2]);
+
+    expect(onPageChangeMock).toHaveBeenCalledWith(expect.objectContaining({ selected: 2 }));
+  });
+});
