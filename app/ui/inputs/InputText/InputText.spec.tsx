a2bf92ee0217828906ab2cfbb02c2f886906c13a
Artem <mstrjesm@gmail.com>
2023-11-02 23:11:25 +0200

    feat(input-text): add test

---
+++ app/ui/inputs/InputText/InputText.spec.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Form, useField } from 'react-final-form';
+import { InputText } from './InputText';
+
+const TestComponent = () => {
+  const { input } = useField('input');
+  return <InputText name="input" label="Your Label" value="" onChange={input.onChange} />;
+};
+
+describe('InputText component', () => {
+  it('renders with the correct label', () => {
+    const { getByLabelText } = render(
+      <Form
+        onSubmit={() => {}}
+        render={() => <TestComponent />}
+      />
+    );
+    const labelElement = getByLabelText('Your Label');
+    expect(labelElement).toBeInTheDocument();
+  });
+
+  it('handles input change correctly', () => {
+    const { getByLabelText } = render(
+      <Form
+        onSubmit={() => {}}
+        render={() => <TestComponent />}
+      />
+    );
+    const inputElement = getByLabelText('Your Label');
+    fireEvent.change(inputElement, { target: { value: 'Test Input' } });
+    expect(inputElement).toHaveValue('Test Input');
+  });
+});
