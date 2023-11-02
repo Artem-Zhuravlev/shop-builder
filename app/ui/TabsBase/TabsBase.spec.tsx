73830763fcfe26a70394e86a0a4975a429d368c6
Artem <mstrjesm@gmail.com>
2023-11-02 23:51:59 +0200

    feat(tabs-base): add test

---
+++ app/ui/TabsBase/TabsBase.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { TabsBase } from './TabsBase';
+
+describe('TabsBase component', () => {
+  const tabs = [
+    { label: 'Tab 1', description: 'Description for Tab 1' },
+    { label: 'Tab 2', description: 'Description for Tab 2' },
+    { label: 'Tab 3', description: 'Description for Tab 3' },
+  ];
+
+  test('renders tabs with correct labels', () => {
+    const { getByText } = render(<TabsBase items={tabs} />);
+
+    tabs.forEach(tab => {
+      expect(getByText(tab.label)).toBeInTheDocument();
+    });
+  });
+
+  test('renders description of active tab', () => {
+    const { getByText } = render(<TabsBase items={tabs} />);
+
+    const activeTabDescription = getByText(tabs[0].description);
+    expect(activeTabDescription).toBeInTheDocument();
+  });
+
+  test('changes active tab on click', () => {
+    const { getByText } = render(<TabsBase items={tabs} />);
+
+    const tabToClick = getByText('Tab 2');
+    fireEvent.click(tabToClick);
+
+    const newActiveTabDescription = getByText(tabs[1].description);
+    expect(newActiveTabDescription).toBeInTheDocument();
+  });
+});
