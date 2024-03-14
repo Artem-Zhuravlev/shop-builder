901fbadf702f58c4729de5a945988836bb25ed3a
Artem <mstrjesm@gmail.com>
2024-03-14 10:57:41 +0200

    fix: styles path for root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Providers } from '@/lib/providers'
+import { Providers } from '@/lib/providers';
 
-import './styles/globals.scss'
+import '@/styles/globals.scss';
 
 export default function RootLayout(props: React.PropsWithChildren) {
-  return (
-    <Providers>
-      <html lang="en">
-        <body>
-          <main>{props.children}</main>
-        </body>
-      </html>
-    </Providers>
-  )
+	return (
+		<Providers>
+			<html lang='en'>
+				<body>
+					<main>{props.children}</main>
+				</body>
+			</html>
+		</Providers>
+	);
 }
