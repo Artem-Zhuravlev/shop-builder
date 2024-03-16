c2c309afdb5d8a2bd5b2bd4a10e04ba764ebad54
Artem <mstrjesm@gmail.com>
2024-03-16 20:26:51 +0200

    refactor(label): styles

--- shared/inputs/Label/Label.tsx
+++ shared/inputs/Label/Label.tsx
@@ -2,63 +2,46 @@ import React, { ReactNode, FC, useState } from 'react';
 import cls from './Label.module.scss';
 
 interface LabelProps {
-  children: ReactNode;
-  name?: string;
-  onSuffixClick?: () => void;
-  hasError?: boolean;
-  error?: string;
+	children: ReactNode;
+	name?: string;
+	onSuffixClick?: () => void;
+	hasError?: boolean;
+	error?: string;
 }
 
-export const Label:FC<LabelProps> = ({
-  children,
-  name,
-  onSuffixClick,
-  hasError,
-  error
+export const Label: FC<LabelProps> = ({
+	children,
+	name,
+	onSuffixClick,
+	hasError,
+	error,
 }) => {
-  const [showEyeIcon, setShowEyeIcon] = useState(true);
+	const [showEyeIcon, setShowEyeIcon] = useState(true);
 
-  const toggleEyeIcon = () => {
-    if (onSuffixClick) {
-      setShowEyeIcon(!showEyeIcon);
-      onSuffixClick();
-    }
-  };
+	const toggleEyeIcon = () => {
+		if (onSuffixClick) {
+			setShowEyeIcon(!showEyeIcon);
+			onSuffixClick();
+		}
+	};
 
-  return (
-    <label
-      className={name && cls.Label}
-    >
-      {name && (
-        <div
-          className={cls.LabelName}
-        >{name}</div>
-      )}
-      {children}
-      {
-        onSuffixClick && (
-          <button
-            type="button"
-            className={cls.LabelSuffix}
-            onClick={toggleEyeIcon}
-          >
-            {
-              showEyeIcon
-              ? (<span className="icon-eye-blocked"></span>) 
-              : (<span className="icon-eye"></span>)
-            }
-          </button>
-        )
-      }
-      {
-        hasError && (
-          <span
-            className={cls.ErrorMessage}
-          >
-            {error}
-          </span>
-        )
-      }
-    </label>
-  )
-}
+	return (
+		<label className={cls.Label}>
+			{name && <div className={cls.LabelName}>{name}</div>}
+			{children}
+			{onSuffixClick && (
+				<button
+					type='button'
+					className={cls.LabelSuffix}
+					onClick={toggleEyeIcon}>
+					{showEyeIcon ? (
+						<span className='icon-eye-blocked'></span>
+					) : (
+						<span className='icon-eye'></span>
+					)}
+				</button>
+			)}
+			{hasError && <span className={cls.ErrorMessage}>{error}</span>}
+		</label>
+	);
+};
