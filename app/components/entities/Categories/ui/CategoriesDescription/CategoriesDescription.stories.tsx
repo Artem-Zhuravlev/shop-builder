926dbc49741eeaffbe31264a692150f5982336e2
Artem <mstrjesm@gmail.com>
2024-04-07 13:19:42 +0300

    feat(categoreis-description): add component

--- app/components/entities/Categories/ui/CategoriesDescription/CategoriesDescription.stories.tsx
+++ app/components/entities/Categories/ui/CategoriesDescription/CategoriesDescription.stories.tsx
@@ -8,10 +8,17 @@ export default {
 } as Meta<typeof CategoriesDescription>;
 
 const Template: StoryFn<typeof CategoriesDescription> = (args) => {
-	return <CategoriesDescription {...args} />;
+	return (
+		<div style={{ maxWidth: '870px' }}>
+			<CategoriesDescription {...args} />
+		</div>
+	);
 };
 
 export const Default = Template.bind({});
 Default.args = {
 	title: 'Desktops',
+	image: 'https://placehold.co/180x108/000000/FFB1F5/jpg',
+	description:
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper auctor neque vitae tempus quam pellentesque. Facilisis leo vel fringilla est ullamcorper. Tellus id interdum velit laoreet id donec ultrices tincidunt. Vitae nunc sed velit dignissim sodales. Mi proin sed libero enim sed. Mi quis hendrerit dolor magna eget est lorem ipsum.',
 };
--- app/components/entities/Categories/ui/CategoriesDescription/CategoriesDescription.tsx
+++ app/components/entities/Categories/ui/CategoriesDescription/CategoriesDescription.tsx
@@ -4,16 +4,27 @@ import cls from './CategoriesDescription.module.scss';
 
 interface CategoriesDescriptionProps {
 	title: string;
+	image: string;
+	description: string;
 }
 
 export const CategoriesDescription: FC<CategoriesDescriptionProps> = (
 	props
 ) => {
-	const { title } = props;
+	const { title, image, description } = props;
 
 	return (
 		<div className={cls.CategoriesDescription}>
 			<h1 className='h2'>{title}</h1>
+			<div className={cls.CategoriesDescriptionContent}>
+				<Image
+					src={image}
+					alt={title}
+					width={108}
+					height={108}
+				/>
+				<p>{description}</p>
+			</div>
 		</div>
 	);
 };
