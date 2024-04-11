44124b789c8b02b64ddafe2eafa77374efe46559
Artem <mstrjesm@gmail.com>
2024-04-11 15:03:28 +0300

    feat(sideba-filter-item): add component

---
+++ app/components/widgets/SidebarFilter/SidebarFilterItem.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { SidebarFilterItem } from './SidebarFilterItem';
+
+export default {
+	title: 'Layout/SidebarFilter/SidebarFilterItem',
+	component: SidebarFilterItem,
+} as Meta<typeof SidebarFilterItem>;
+
+const Template: StoryFn<typeof SidebarFilterItem> = (args) => {
+	return (
+		<div
+			style={{
+				maxWidth: 270,
+				border: '1px solid var(--gray-40)',
+				padding: 'var(--size-small) var(--space-medium)',
+			}}>
+			<SidebarFilterItem {...args} />
+		</div>
+	);
+};
+
+const children = <div>Test content test content some mock data</div>;
+
+export const Default = Template.bind({});
+Default.args = {
+	amount: 10,
+	children,
+};
+
+export const NoAmount = Template.bind({});
+NoAmount.args = {
+	children,
+};
---
+++ app/components/widgets/SidebarFilter/SidebarFilterItem.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactNode } from 'react';
+import cls from './SidebarFilter.module.scss';
+
+interface SidebarFilterItemProps {
+	children?: ReactNode;
+	amount?: number;
+}
+
+export const SidebarFilterItem: FC<SidebarFilterItemProps> = (props) => {
+	const { children, amount = 0 } = props;
+
+	return (
+		<div className={cls.SidebarFilterItem}>
+			{children}
+			{amount > 0 && (
+				<span className={cls.SidebarFilterItemAmount}>{amount}</span>
+			)}
+		</div>
+	);
+};
