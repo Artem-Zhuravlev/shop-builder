c785b20bd6f498b3a4b2abcf34b1e36f4564a5b3
Artem <mstrjesm@gmail.com>
2024-04-24 20:14:48 +0300

    feat(product-info-row): add test

---
+++ app/components/entities/Product/ui/ProductInfo/common/ProductInfoRow/ProductInfoRow.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { customRender } from '@utils/intlWrapper/IntlWrapper';
+import { ProductInfoRow } from './ProductInfoRow';
+
+describe('ProductInfoRow', () => {
+	it('renders children correctly', () => {
+		const { getByText } = customRender(
+			<ProductInfoRow>
+				<span>Test Child</span>
+			</ProductInfoRow>
+		);
+		expect(getByText('Test Child')).toBeInTheDocument();
+	});
+
+	it('applies custom className correctly', () => {
+		const { container } = customRender(
+			<ProductInfoRow className='custom-class'>
+				<span>Test Child</span>
+			</ProductInfoRow>
+		);
+		expect(container.firstChild).toHaveClass('custom-class');
+	});
+});
