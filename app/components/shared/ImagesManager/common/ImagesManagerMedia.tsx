dabb504bd8787b8c0c9f73d67475ce429e102cce
Artem <mstrjesm@gmail.com>
2024-06-11 18:48:23 +0300

    feat(images-manager-media): add main content

--- app/components/shared/ImagesManager/common/ImagesManagerMedia.tsx
+++ app/components/shared/ImagesManager/common/ImagesManagerMedia.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import cls from '../ImagesManager.module.scss';
 import {
 	ImagesManagerMediaFilter,
@@ -7,15 +7,59 @@ import {
 
 interface ImagesManagerMediaProps {}
 
+interface Image {
+	id: number;
+	url: string;
+	alt: string;
+}
+
 export const ImagesManagerMedia: FC<ImagesManagerMediaProps> = (props) => {
+	const [images, setImages] = useState<Image[]>([]);
 	const handleFilters = (value: ImageFilterProps) => {
 		console.log(value);
+	};
+
+	useEffect(() => {
+		setImages([
+			{
+				id: 1,
+				url: 'https://picsum.photos/200/300',
+				alt: 'image',
+			},
+			{
+				id: 2,
+				url: 'https://picsum.photos/200/300',
+				alt: 'image 2',
+			},
+			{
+				id: 3,
+				url: 'https://picsum.photos/200/300',
+				alt: 'image 3',
+			},
+		]);
+	}, []);
+
+	const handleFileChange = (image: Image) => {
+		// TODO: Image details by image id
 	};
 
 	return (
 		<div className={cls.ImagesManagerMedia}>
 			<div className={cls.ImagesManagerMediaMain}>
 				<ImagesManagerMediaFilter onSelect={handleFilters} />
+				<div className={cls.ImagesManagerMediaContent}>
+					{images.map((image) => (
+						<button
+							key={image.id}
+							className={cls.ImagesManagerMediaItem}
+							onClick={() => handleFileChange(image)}>
+							<img
+								src={image.url}
+								alt={image.alt}
+							/>
+						</button>
+					))}
+				</div>
 			</div>
 			<div className={cls.ImagesManagerMediaSidebar}></div>
 		</div>
