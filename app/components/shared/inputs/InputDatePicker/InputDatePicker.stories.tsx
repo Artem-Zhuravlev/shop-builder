2f0ca890ea93763c5859f5d34e2680d56484e0b7
Artem <mstrjesm@gmail.com>
2024-06-17 16:49:43 +0300

    refactor(input-date-picker): for react-final-form

--- app/components/shared/inputs/InputDatePicker/InputDatePicker.stories.tsx
+++ app/components/shared/inputs/InputDatePicker/InputDatePicker.stories.tsx
@@ -1,32 +1,50 @@
 import React, { useState } from 'react';
 import { StoryFn, Meta } from '@storybook/react';
-import { InputDatePicker } from './InputDatePicker';
+import { InputDatePicker, InputDatePickerProps } from './InputDatePicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { Form } from 'react-final-form';
+import { ButtonBase } from '@shared/ButtonBase';
+import { action } from '@storybook/addon-actions';
 
 export default {
 	title: 'Shared/Inputs/InputDatePicker',
 	component: InputDatePicker,
 } as Meta<typeof InputDatePicker>;
 
-const Template: StoryFn<typeof InputDatePicker> = (args) => {
-	const [dateValue, setDateValue] = useState(args.selected);
+interface InputStoryProps extends InputDatePickerProps {
+	field?: string | Date;
+}
+
+const Template: StoryFn<InputStoryProps> = (args) => {
+	const onSubmit = async (values: object) => {
+		action('onSubmit')(values);
+	};
 
 	return (
 		<div style={{ width: '300px' }}>
-			<InputDatePicker
-				{...args}
-				selected={dateValue}
-				onChange={(date: Date | null) => {
-					setDateValue(date);
-				}}
-			/>
+			<Form
+				onSubmit={onSubmit}
+				initialValues={{ field: '' }}>
+				{({ handleSubmit }) => (
+					<form
+						onSubmit={handleSubmit}
+						style={{
+							display: 'flex',
+							gap: '20px',
+							flexWrap: 'wrap',
+						}}>
+						<InputDatePicker {...args} />
+						<ButtonBase type='submit'>Submit</ButtonBase>
+					</form>
+				)}
+			</Form>
 		</div>
 	);
 };
 
 export const Default = Template.bind({});
 Default.args = {
-	selected: new Date() as Date,
-	placeholderText: 'Datepicker placeholder',
-	dateFormat: 'MMMM d, yyyy h:mm aa',
+	name: 'field',
+	placeholder: 'Datepicker',
+	required: true,
 };
--- app/components/shared/inputs/InputDatePicker/InputDatePicker.tsx
+++ app/components/shared/inputs/InputDatePicker/InputDatePicker.tsx
@@ -1,19 +1,77 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import classNames from 'classnames';
+import { useTranslations } from 'next-intl';
 import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import { Field } from 'react-final-form';
+import { getValidationMessage } from '@utils/validations';
+import { ValidationHandler } from '@utils/validations/types';
+import { Label } from '../Label/Label';
 import cls from './InputDatePicker.module.scss';
 
-export const InputDatePicker: FC<ReactDatePickerProps> = (props) => {
-	const { dateFormat, placeholderText, selected, onChange } = props;
+export interface InputDatePickerProps extends ReactDatePickerProps {
+	className?: string;
+	name: string;
+	required?: boolean;
+	validationHandler?: ValidationHandler;
+	placeholder: string;
+}
+
+export const InputDatePicker: FC<InputDatePickerProps> = (props) => {
+	const {
+		className,
+		dateFormat = 'yyyy-MM-dd',
+		name,
+		placeholder,
+		required = false,
+		validationHandler,
+	} = props;
+	const t = useTranslations();
+	const [error, setError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const inputClasses = classNames(cls.InputDatePicker, {
+		[cls.InputTextError]: error,
+	});
+
+	const renderInputField = ({ input, meta }: any) => {
+		setError(!!meta.error && meta.touched && meta.submitFailed);
+		setErrorMessage(meta.error || '');
+
+		return (
+			<>
+				<ReactDatePicker
+					selected={input.value}
+					onFocus={(e) => input.onFocus(e)}
+					onChange={(e) => input.onChange(e)}
+					onBlur={(e) => input.onBlur(e)}
+					className={inputClasses}
+					closeOnScroll
+					popperClassName={cls.Popper}
+					placeholderText={required ? `${placeholder} *` : placeholder}
+					dateFormat={dateFormat}
+				/>
+			</>
+		);
+	};
 
 	return (
-		<ReactDatePicker
-			selected={selected}
-			onChange={onChange}
-			className={cls.InputDatePicker}
-			popperClassName={cls.Popper}
-			placeholderText={placeholderText}
-			dateFormat={dateFormat}
-		/>
+		<Label
+			hasError={error}
+			error={errorMessage}
+			className={className}>
+			<Field
+				name={name}
+				validate={(value) => {
+					getValidationMessage(
+						value.toString(),
+						required,
+						t,
+						validationHandler
+					);
+				}}>
+				{renderInputField}
+			</Field>
+		</Label>
 	);
 };
 
