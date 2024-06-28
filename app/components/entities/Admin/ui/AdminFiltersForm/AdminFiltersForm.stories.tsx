be29a64202b577e22db7355f384d695b6c4e7c1b
Artem <mstrjesm@gmail.com>
2024-06-28 19:35:48 +0300

    feat(admin-filters-form): add story

---
+++ app/components/entities/Admin/ui/AdminFiltersForm/AdminFiltersForm.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { AdminFiltersForm } from './AdminFiltersForm';
+
+export default {
+  title: 'Entities/Admin/Filters/AdminFiltersForm',
+  component: AdminFiltersForm,
+} as Meta<typeof AdminFiltersForm>;
+
+const Template: StoryFn<typeof AdminFiltersForm> = (args) => {
+  return <AdminFiltersForm {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
