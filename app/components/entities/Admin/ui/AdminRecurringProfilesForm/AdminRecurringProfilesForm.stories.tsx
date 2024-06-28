2420d0848f09e5517b6cf28902c21d951ef8eda4
Artem <mstrjesm@gmail.com>
2024-06-28 17:49:37 +0300

    fix(admin-recurring-profiles-form): path for storybook structure

--- app/components/entities/Admin/ui/AdminRecurringProfilesForm/AdminRecurringProfilesForm.stories.tsx
+++ app/components/entities/Admin/ui/AdminRecurringProfilesForm/AdminRecurringProfilesForm.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryFn } from '@storybook/react';
 import { AdminRecurringProfilesForm } from './AdminRecurringProfilesForm';
 
 export default {
-  title: 'Entities/Admin/AdminRecurringProfilesForm/AdminRecurringProfilesForm',
+  title: 'Entities/Admin/Recurring Profiles/AdminRecurringProfilesForm',
   component: AdminRecurringProfilesForm,
 } as Meta<typeof AdminRecurringProfilesForm>;
 
