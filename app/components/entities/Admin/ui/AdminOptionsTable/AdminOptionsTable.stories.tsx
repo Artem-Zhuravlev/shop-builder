9db539f4a58e8641bac4de6881408a08f32655c3
Artem <mstrjesm@gmail.com>
2024-07-01 18:11:13 +0300

    feat(admin-options-table): add story

---
+++ app/components/entities/Admin/ui/AdminOptionsTable/AdminOptionsTable.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { AdminOptionsTable } from './AdminOptionsTable';
+
+export default {
+  title: 'Entities/Admin/Options/AdminOptionsTable',
+  component: AdminOptionsTable,
+} as Meta<typeof AdminOptionsTable>;
+
+const Template: StoryFn<typeof AdminOptionsTable> = (args) => {
+  return <AdminOptionsTable {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  nodes: [
+    {
+      id: 1,
+      sort_order: 1,
+      option_name: 'Checkbox',
+      slug: 'checkbox',
+    },
+    {
+      id: 2,
+      sort_order: 2,
+      option_name: 'Color',
+      slug: 'color',
+    },
+    {
+      id: 3,
+      sort_order: 4,
+      option_name: 'Date',
+      slug: 'date',
+    },
+    {
+      id: 4,
+      sort_order: 5,
+      option_name: 'Date & Time',
+      slug: 'date-and-time',
+    },
+  ],
+};
