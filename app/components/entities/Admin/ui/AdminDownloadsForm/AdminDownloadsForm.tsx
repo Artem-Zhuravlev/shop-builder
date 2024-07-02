6769b5ee5c1be5e939f1b8fdea35ba5121e5d958
Artem <mstrjesm@gmail.com>
2024-07-02 17:13:05 +0300

    feat(admin-downloads-form): add component

---
+++ app/components/entities/Admin/ui/AdminDownloadsForm/AdminDownloadsForm.tsx
@@ -0,0 +1,51 @@
+import { LanguageTabs } from '@features/tabs';
+import { InputFile, InputText } from '@shared/inputs';
+import { FormLayout } from '@widgets/FormLayout';
+import { useTranslations } from 'next-intl';
+import { FC, useState } from 'react';
+import { Form } from 'react-final-form';
+
+export const AdminDownloadsForm: FC = () => {
+  const t = useTranslations();
+  const [language, setLanguage] = useState<'en' | 'ua'>('en');
+
+  const onSubmit = (value: any) => {
+    console.log(value);
+  };
+
+  return (
+    <>
+      <LanguageTabs
+        items={['en', 'ua']}
+        onSelect={(language) => setLanguage(language as 'en' | 'ua')}
+      />
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => {
+          return (
+            <FormLayout
+              className='row align-items-center'
+              onSubmit={handleSubmit}
+              title={t('base.downloads')}>
+              <InputText
+                className='col-md-4'
+                name={`${language}.download_name`}
+                placeholder={t('inputs.download_name')}
+                required
+              />
+              <InputFile className='col-md-4' name='filename' required />
+              <InputText
+                className='col-md-4'
+                name='mask'
+                placeholder={t('inputs.mask')}
+                required
+              />
+            </FormLayout>
+          );
+        }}
+      />
+    </>
+  );
+};
+
+AdminDownloadsForm.displayName = 'AdminDownloadsForm';
