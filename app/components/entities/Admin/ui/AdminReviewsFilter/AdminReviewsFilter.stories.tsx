b26b8f63de75bd28839b5f1ad375ad2969137d82
Artem <mstrjesm@gmail.com>
2024-07-02 21:05:43 +0300

    feat(admin-reviews-filter): add story

---
+++ app/components/entities/Admin/ui/AdminReviewsFilter/AdminReviewsFilter.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { AdminReviewsFilter } from './AdminReviewsFilter';
+
+export default {
+  title: 'Entities/Admin/Reviews/AdminReviewsFilter',
+  component: AdminReviewsFilter,
+} as Meta<typeof AdminReviewsFilter>;
+
+const Template: StoryFn<typeof AdminReviewsFilter> = (args) => {
+  return <AdminReviewsFilter {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
