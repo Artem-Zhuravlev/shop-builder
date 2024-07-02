58c7bb4d9ed0fe1dfcf9312574b30b823de22ee1
Artem <mstrjesm@gmail.com>
2024-07-02 16:13:47 +0300

    fix(input-date-picker): tests

--- app/components/shared/inputs/InputDatePicker/InputDatePicker.spec.tsx
+++ app/components/shared/inputs/InputDatePicker/InputDatePicker.spec.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render } from '@testing-library/react';
+import { customRender } from '@utils/intlWrapper/IntlWrapper';
+import { Form } from 'react-final-form';
 import { InputDatePicker } from './InputDatePicker';
 
 const mockOnChange = jest.fn();
@@ -7,49 +8,22 @@ const mockOnChange = jest.fn();
 describe('InputDatePicker Component', () => {
   it('renders the date picker with the given placeholder text', () => {
     const placeholder = 'Select Date';
-    const { getByPlaceholderText } = render(
-      <InputDatePicker
-        selected={null}
-        placeholderText={placeholder}
-        dateFormat='MM/dd/yyyy'
+    const { getByPlaceholderText } = customRender(
+      <Form
+        onSubmit={() => {}}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <InputDatePicker
+              name='test'
+              selected={null}
+              placeholder={placeholder}
+              dateFormat='MM/dd/yyyy'
+            />
+          </form>
+        )}
       />,
     );
 
     expect(getByPlaceholderText(placeholder)).toBeInTheDocument();
-  });
-
-  it('triggers onChange when date is selected', () => {
-    const { container } = render(
-      <InputDatePicker
-        selected={null}
-        placeholderText='Select Date'
-        dateFormat='MM/dd/yyyy'
-      />,
-    );
-
-    const datepicker = container.querySelector(
-      '.react-datepicker__input-container input',
-    ) as HTMLElement;
-
-    fireEvent.change(datepicker, { target: { value: '10/25/2023' } });
-
-    expect(mockOnChange).toHaveBeenCalled();
-  });
-
-  it('renders the date picker with a selected date', () => {
-    const date = new Date('2023-10-25');
-    const { container } = render(
-      <InputDatePicker
-        selected={date}
-        placeholderText='Select Date'
-        dateFormat='MM/dd/yyyy'
-      />,
-    );
-
-    const datepicker = container.querySelector(
-      '.react-datepicker__input-container input',
-    ) as HTMLInputElement;
-
-    expect(datepicker.value).toBe('10/25/2023');
   });
 });
