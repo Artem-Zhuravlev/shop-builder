e19c70c86178eecf85f838b09cd69636951fa788
Artem <mstrjesm@gmail.com>
2024-07-02 17:13:19 +0300

    feat(admin-downloads-form): add story

---
+++ app/components/entities/Admin/ui/AdminDownloadsForm/AdminDownloadsForm.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { AdminDownloadsForm } from './AdminDownloadsForm';
+
+export default {
+  title: 'Entities/Admin/Downloads/AdminDownloadsForm',
+  component: AdminDownloadsForm,
+} as Meta<typeof AdminDownloadsForm>;
+
+const Template: StoryFn<typeof AdminDownloadsForm> = (args) => {
+  return <AdminDownloadsForm {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
