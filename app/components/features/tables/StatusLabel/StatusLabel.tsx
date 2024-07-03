6b065b834b0ba08dbf25dcff03b59e7382e559b5
Artem <mstrjesm@gmail.com>
2024-07-03 20:09:26 +0300

    refactor(status-label): types

--- app/components/features/tables/StatusLabel/StatusLabel.tsx
+++ app/components/features/tables/StatusLabel/StatusLabel.tsx
@@ -3,8 +3,10 @@ import { useTranslations } from 'next-intl';
 import { FC } from 'react';
 import cls from './StatusLabel.module.scss';
 
+export type Status = 'pending' | 'cancelled' | 'delivered' | 'processing' | 'shipped'
+
 interface StatusLabelProps {
-  value: 'pending' | 'cancelled' | 'delivered' | 'processing' | 'shipped';
+  value: Status;
 }
 
 export const StatusLabel: FC<StatusLabelProps> = ({ value }) => {
