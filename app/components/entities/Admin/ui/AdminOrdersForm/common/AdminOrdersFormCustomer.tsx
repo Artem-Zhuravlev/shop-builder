6e2c8f9b641cbc02a028123d3c3093b5a33c8c8b
Artem <mstrjesm@gmail.com>
2024-07-05 17:38:23 +0300

    feat(admin-orders-form-customer): add component

---
+++ app/components/entities/Admin/ui/AdminOrdersForm/common/AdminOrdersFormCustomer.tsx
@@ -0,0 +1,59 @@
+import { InputDatePicker, InputText } from '@shared/inputs';
+import { emailValidate, phoneValidate } from '@utils/validations';
+import { useTranslations } from 'next-intl';
+import { FC } from 'react';
+
+export const AdminOrdersFormCustomer: FC = () => {
+  const t = useTranslations();
+
+  return (
+    <>
+      <h3 className='h4'>{t('base.customer')}</h3>
+      <InputText
+        className='col-md-4'
+        name='customer'
+        placeholder={t('inputs.customer')}
+        required
+      />
+      <InputText
+        className='col-md-4'
+        name='customer_group'
+        placeholder={t('inputs.customer_group')}
+        required
+      />
+      <InputText
+        className='col-md-4'
+        name='first_name'
+        placeholder={t('inputs.first_name')}
+        required
+      />
+      <InputText
+        className='col-md-4'
+        name='last_name'
+        placeholder={t('inputs.last_name')}
+        required
+      />
+      <InputText
+        className='col-md-4'
+        name='email'
+        placeholder={t('inputs.email')}
+        required
+        validationHandler={emailValidate}
+      />
+      <InputText
+        className='col-md-4'
+        name='phone'
+        placeholder={t('inputs.phone')}
+        required
+        validationHandler={phoneValidate}
+      />
+      <InputDatePicker
+        className='col-md-4'
+        name='date_added'
+        placeholder={t('inputs.date_added')}
+      />
+    </>
+  );
+};
+
+AdminOrdersFormCustomer.displayName = 'AdminOrdersFormCustomer';
