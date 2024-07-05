ea38fc4d61f2326aa3b78c5e412f61655513ad10
Artem <mstrjesm@gmail.com>
2024-07-05 18:29:22 +0300

    feat(status-label): add stories for statuses

--- app/components/features/tables/StatusLabel/StatusLabel.stories.tsx
+++ app/components/features/tables/StatusLabel/StatusLabel.stories.tsx
@@ -18,6 +18,30 @@ export const Cancelled = Template.bind({});
 Cancelled.args = {
   value: 'cancelled',
 };
+export const CancelledReversal = Template.bind({});
+CancelledReversal.args = {
+  value: 'canceled_reversal',
+};
+export const Chargeback = Template.bind({});
+Chargeback.args = {
+  value: 'chargeback',
+};
+export const Denied = Template.bind({});
+Denied.args = {
+  value: 'denied',
+};
+export const Expired = Template.bind({});
+Expired.args = {
+  value: 'expired',
+};
+export const Failed = Template.bind({});
+Failed.args = {
+  value: 'failed',
+};
+export const Complete = Template.bind({});
+Complete.args = {
+  value: 'complete',
+};
 export const Delivered = Template.bind({});
 Delivered.args = {
   value: 'delivered',
@@ -26,7 +50,19 @@ export const Processing = Template.bind({});
 Processing.args = {
   value: 'processing',
 };
+export const Processed = Template.bind({});
+Processed.args = {
+  value: 'processed',
+};
+export const Refunded = Template.bind({});
+Refunded.args = {
+  value: 'refunded',
+};
 export const Shipped = Template.bind({});
 Shipped.args = {
   value: 'shipped',
+};
+export const Voided = Template.bind({});
+Voided.args = {
+  value: 'voided',
 };
