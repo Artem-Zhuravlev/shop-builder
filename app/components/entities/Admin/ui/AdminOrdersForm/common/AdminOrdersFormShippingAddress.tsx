aee44fb25934de9359bc4dd5d5eeade5d0605af8
Artem <mstrjesm@gmail.com>
2024-07-05 17:25:33 +0300

    feat(admin-orders-form-shipping-address): add component

---
+++ app/components/entities/Admin/ui/AdminOrdersForm/common/AdminOrdersFormShippingAddress.tsx
@@ -0,0 +1,62 @@
+import { InputSelect, InputText } from '@shared/inputs';
+import { useTranslations } from 'next-intl';
+import { FC } from 'react';
+
+export const AdminOrdersFormShippingAddress: FC = () => {
+  const t = useTranslations();
+
+  return (
+    <>
+      <h3 className='h4'>{t('base.shipping_address')}</h3>
+      <InputText
+        className='col-md-4'
+        name='shipping_address.first_name'
+        placeholder={t('inputs.first_name')}
+      />
+      <InputText
+        className='col-md-4'
+        name='shipping_address.last_name'
+        placeholder={t('inputs.last_name')}
+      />
+      <InputText
+        className='col-md-4'
+        name='shipping_address.company'
+        placeholder={t('inputs.company')}
+      />
+      <InputText
+        className='col-md-4'
+        name='shipping_address.address_1'
+        placeholder={t('inputs.address_1')}
+      />
+      <InputText
+        className='col-md-4'
+        name='shipping_address.address_2'
+        placeholder={t('inputs.address_2')}
+      />
+      <InputText
+        className='col-md-4'
+        name='shipping_address.city'
+        placeholder={t('inputs.city')}
+      />
+      <InputText
+        className='col-md-4'
+        name='shipping_address.postcode'
+        placeholder={t('inputs.postcode')}
+      />
+      <InputSelect
+        className='col-md-4'
+        name='shipping_address.country'
+        placeholder={t('inputs.country')}
+        options={[{ value: 'algeria', label: 'Algeria' }]}
+      />
+      <InputSelect
+        className='col-md-4'
+        name='shipping_address.region'
+        placeholder={t('inputs.region')}
+        options={[{ value: 'agdam', label: 'Agdam' }]}
+      />
+    </>
+  );
+};
+
+AdminOrdersFormShippingAddress.displayName = 'AdminOrdersFormShippingAddress';
