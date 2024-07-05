3e2c95ed9766a3d36bebdd747876589ba02aca76
Artem <mstrjesm@gmail.com>
2024-07-05 17:28:23 +0300

    feat: add imports for admin-orders-parts

---
+++ app/components/entities/Admin/ui/AdminOrdersForm/common/index.ts
@@ -0,0 +1,4 @@
+export { AdminOrdersFormCustomer } from './AdminOrdersFormCustomer';
+export { AdminOrdersFormPaymentAddress } from './AdminOrdersFormPaymentAddress';
+export { AdminOrdersFormProducts } from './AdminOrdersFormProducts';
+export { AdminOrdersFormShippingAddress } from './AdminOrdersFormShippingAddress';
