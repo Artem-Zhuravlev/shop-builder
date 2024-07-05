5a705b28390f157061be729335714da26f32760a
Artem <mstrjesm@gmail.com>
2024-07-05 18:33:40 +0300

    feat(input-status): add story

---
+++ app/components/shared/inputs/InputStatus/InputStatus.stories.tsx
@@ -0,0 +1,34 @@
+import { AutoSave } from '@shared/inputs';
+import { action } from '@storybook/addon-actions';
+import { Meta, StoryFn } from '@storybook/react';
+import { Form } from 'react-final-form';
+import { InputStatus } from './InputStatus';
+
+export default {
+  title: 'Shared/Inputs/InputStatus',
+  component: InputStatus,
+} as Meta<typeof InputStatus>;
+
+const Template: StoryFn<typeof InputStatus> = (args) => {
+  const onSubmit = (values: any) => {
+    action('onSubmit')(values);
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      {({ handleSubmit }) => (
+        <>
+          <div>
+            <AutoSave debounce={0} save={handleSubmit} />
+            <InputStatus {...args} />
+          </div>
+        </>
+      )}
+    </Form>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'status',
+};
