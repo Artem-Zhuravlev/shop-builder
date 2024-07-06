fc9d277ffce8ba661d4d4306b65acdcdbb31fb6a
Artem <mstrjesm@gmail.com>
2024-07-06 16:43:36 +0300

    feat(admin-orders-filter): add story

---
+++ app/components/entities/Admin/ui/AdminOrdersFilter/AdminOrdersFilter.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { AdminOrdersFilter } from './AdminOrdersFilter';
+
+export default {
+  title: 'Entities/Admin/Orders/AdminOrdersFilter',
+  component: AdminOrdersFilter,
+} as Meta<typeof AdminOrdersFilter>;
+
+const Template: StoryFn<typeof AdminOrdersFilter> = (args) => {
+  return <AdminOrdersFilter {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
