2165ca33a4926e797e4838b3b7f0c47fe61d8499
Artem <mstrjesm@gmail.com>
2024-07-06 15:36:36 +0300

    refactor(useFormattedDate): add jsdoc, remove unused hooks

--- hooks/formatDate/index.ts
+++ hooks/formatDate/index.ts
@@ -1,11 +1,27 @@
 import moment from 'moment';
-import { useMemo } from 'react';
 
-export function useFormattedDate(date: string): string {
-  const formattedDate = useMemo(() => {
-    if (!date) return '';
-    return moment(date).format('DD.MM.YYYY');
-  }, [date]);
+/**
+ * Object containing different date format strings.
+ * @constant
+ * @type {Object.<string, string>}
+ */
+const FORMATS_TO_DISPLAY = {
+  time: 'HH:mm', // Format for displaying time
+  datetime: 'MMM YYYY HH:mm', // Format for displaying datetime
+  date: 'DD.MM.YYYY', // Format for displaying date
+  month: 'MMM YYYY', // Format for displaying month
+  year: 'YYYY', // Format for displaying year
+};
 
-  return formattedDate;
+/**
+ * Function to format a date string based on a specified format type.
+ * @param {string} date - The input date string to be formatted.
+ * @param {('time'|'datetime'|'date'|'month'|'year')} [type='date'] - The type of format to apply.
+ * @returns {string} The formatted date string.
+ */
+export function useFormattedDate(
+  date: string,
+  type: keyof typeof FORMATS_TO_DISPLAY = 'date',
+): string {
+  return moment(date).format(FORMATS_TO_DISPLAY[type]);
 }
