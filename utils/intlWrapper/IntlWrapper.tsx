f4bb1c44f2439ed62195b012efe89834ddbc4f37
Artem <mstrjesm@gmail.com>
2024-07-08 18:52:36 +0300

    refactor(intl-wrapper): fix imports for jest intl provider

--- utils/intlWrapper/IntlWrapper.tsx
+++ utils/intlWrapper/IntlWrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
-import { ReactElement } from 'react';
+import { render, type RenderResult } from '@testing-library/react';
+import type { ReactElement } from 'react';
 import { Providers } from '@lib/providers';
 import { NextIntlProvider } from 'next-intl';
 import en from '../../messages/en';
@@ -8,11 +8,9 @@ import en from '../../messages/en';
 export const customRender = (ui: ReactElement): RenderResult => {
 	return render(
 		<Providers>
-			<NextIntlProvider
-				locale='en'
-				messages={en}>
+			<NextIntlProvider locale='en' messages={en}>
 				{ui}
 			</NextIntlProvider>
-		</Providers>
+		</Providers>,
 	);
 };
