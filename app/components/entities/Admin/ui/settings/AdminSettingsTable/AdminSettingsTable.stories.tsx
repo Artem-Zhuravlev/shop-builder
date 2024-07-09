18ab79fa6c669ef0d6120ef0c8ae18bdbbd3de1e
Artem <mstrjesm@gmail.com>
2024-07-09 19:11:20 +0300

    feat(admin-settings-table): add component and story

---
+++ app/components/entities/Admin/ui/settings/AdminSettingsTable/AdminSettingsTable.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import { AdminSettingsTable } from './AdminSettingsTable';
+
+export default {
+	title: 'Entities/Admin/Settings/AdminSettingsTable',
+	component: AdminSettingsTable,
+} as Meta<typeof AdminSettingsTable>;
+
+const Template: StoryFn<typeof AdminSettingsTable> = (args) => {
+	return <AdminSettingsTable {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+	nodes: [
+		{
+			id: 1,
+			store_name: 'Your Store (Default)',
+			store_url: 'https://test.com',
+			slug: 'default',
+		},
+	],
+};
---
+++ app/components/entities/Admin/ui/settings/AdminSettingsTable/AdminSettingsTable.tsx
@@ -0,0 +1,100 @@
+import { ButtonBase } from '@shared/ButtonBase';
+import {
+	defaultSortIcon,
+	defaultTableStyles,
+} from '@shared/TableBase/TableBase';
+import type { TableNode } from '@table-library/react-table-library';
+import { getTheme } from '@table-library/react-table-library/baseline';
+import {
+	CellSelect,
+	HeaderCellSelect,
+	useRowSelect,
+} from '@table-library/react-table-library/select';
+import {
+	HeaderCellSort,
+	useSort,
+} from '@table-library/react-table-library/sort';
+import {
+	Body,
+	Cell,
+	Header,
+	HeaderCell,
+	HeaderRow,
+	Row,
+	Table,
+} from '@table-library/react-table-library/table';
+import { useTheme } from '@table-library/react-table-library/theme';
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/router';
+import type { FC } from 'react';
+
+interface AdminSettingsTableItem extends TableNode {
+	id: string | number;
+	store_name: string;
+	store_url: string;
+	slug: string;
+}
+
+interface AdminSettingsTableProps {
+	nodes: AdminSettingsTableItem[];
+}
+
+export const AdminSettingsTable: FC<AdminSettingsTableProps> = (props) => {
+	const { nodes } = props;
+	const data = { nodes };
+	const t = useTranslations('admin');
+	const router = useRouter();
+	const theme = useTheme({
+		...getTheme(),
+		Table: '--data-table-library_grid-template-columns:  50px 1fr 2fr 150px;',
+		...defaultTableStyles,
+	});
+
+	const select = useRowSelect(data, {
+		onChange: onSelectChange,
+	});
+
+	function onSortChange(action: any, state: any) {
+		console.log(action, state);
+	}
+
+	function onSelectChange(action: any, state: any) {
+		console.log(action, state);
+	}
+
+	return (
+		<Table data={data} theme={theme} layout={{ custom: true }} select={select}>
+			{(tableList: AdminSettingsTableItem[]) => (
+				<>
+					<Header>
+						<HeaderRow>
+							<HeaderCellSelect />
+							<HeaderCell>{t('store_name')}</HeaderCell>
+							<HeaderCell>{t('store_url')}</HeaderCell>
+							<HeaderCell>{t('action')}</HeaderCell>
+						</HeaderRow>
+					</Header>
+
+					<Body>
+						{tableList.map((item) => (
+							<Row item={item} key={item.id}>
+								<CellSelect item={item} />
+								<Cell>{item.store_name}</Cell>
+								<Cell>{item.store_url}</Cell>
+								<Cell>
+									<ButtonBase
+										onClick={() => router.push(`/admin/settings/${item.slug}`)}
+									>
+										<span className='icon-pencil' />
+									</ButtonBase>
+								</Cell>
+							</Row>
+						))}
+					</Body>
+				</>
+			)}
+		</Table>
+	);
+};
+
+AdminSettingsTable.displayName = 'AdminSettingsTable';
