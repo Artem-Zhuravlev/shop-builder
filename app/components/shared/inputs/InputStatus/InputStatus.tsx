8b621e92f24be5b3f98847726124ed49443ce96e
Artem <mstrjesm@gmail.com>
2024-07-10 15:17:25 +0300

    feat(input-status): add displayname

--- app/components/shared/inputs/InputStatus/InputStatus.tsx
+++ app/components/shared/inputs/InputStatus/InputStatus.tsx
@@ -3,36 +3,38 @@ import type { FC } from 'react';
 import { InputSelect } from '../InputSelect/InputSelect';
 
 interface InputStatusProps {
-  className?: string;
-  name: string;
+	className?: string;
+	name: string;
 }
 
 export const InputStatus: FC<InputStatusProps> = (props) => {
-  const { className, name } = props;
-  const t = useTranslations();
-  const statuses = [
-    { value: 'canceled', label: t('base.canceled') },
-    { value: 'canceled_reversal', label: t('base.canceled_reversal') },
-    { value: 'chargeback', label: t('base.chargeback') },
-    { value: 'complete', label: t('base.complete') },
-    { value: 'denied', label: t('base.denied') },
-    { value: 'expired', label: t('base.expired') },
-    { value: 'failed', label: t('base.failed') },
-    { value: 'pending', label: t('base.pending') },
-    { value: 'processed', label: t('base.processed') },
-    { value: 'processing', label: t('base.processing') },
-    { value: 'refunded', label: t('base.refunded') },
-    { value: 'reversed', label: t('base.reversed') },
-    { value: 'shipped', label: t('base.shipped') },
-    { value: 'voided', label: t('base.voided') },
-  ];
+	const { className, name } = props;
+	const t = useTranslations();
+	const statuses = [
+		{ value: 'canceled', label: t('base.canceled') },
+		{ value: 'canceled_reversal', label: t('base.canceled_reversal') },
+		{ value: 'chargeback', label: t('base.chargeback') },
+		{ value: 'complete', label: t('base.complete') },
+		{ value: 'denied', label: t('base.denied') },
+		{ value: 'expired', label: t('base.expired') },
+		{ value: 'failed', label: t('base.failed') },
+		{ value: 'pending', label: t('base.pending') },
+		{ value: 'processed', label: t('base.processed') },
+		{ value: 'processing', label: t('base.processing') },
+		{ value: 'refunded', label: t('base.refunded') },
+		{ value: 'reversed', label: t('base.reversed') },
+		{ value: 'shipped', label: t('base.shipped') },
+		{ value: 'voided', label: t('base.voided') },
+	];
 
-  return (
-    <InputSelect
-      className={className}
-      name={name}
-      options={statuses}
-      placeholder={t('inputs.status')}
-    />
-  );
+	return (
+		<InputSelect
+			className={className}
+			name={name}
+			options={statuses}
+			placeholder={t('inputs.status')}
+		/>
+	);
 };
+
+InputStatus.displayName = 'InputStatus';
