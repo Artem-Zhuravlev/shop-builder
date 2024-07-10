8b0aa9b0a727cd4ef5d305b7cb138813f524b45a
Artem <mstrjesm@gmail.com>
2024-07-10 16:42:40 +0300

    feat(admin-settings-store-form): add form

---
+++ app/components/entities/Admin/ui/settings/AdminSettingsForm/common/AdminSettigsStoreForm.tsx
@@ -0,0 +1,69 @@
+import { InputImage, InputText, InputTextarea } from '@shared/inputs';
+import { emailValidate, phoneValidate } from '@utils/validations';
+import { FormLayout } from '@widgets/FormLayout';
+import { useTranslations } from 'next-intl';
+import type { FC } from 'react';
+import { Form } from 'react-final-form';
+
+export const AdminSettingsStoreForm: FC = () => {
+	const t = useTranslations();
+
+	const onSubmit = (values: any) => {
+		console.log('Form Submitted:', values);
+	};
+
+	return (
+		<>
+			<Form onSubmit={onSubmit}>
+				{({ handleSubmit }) => (
+					<FormLayout
+						className='row align-items-center'
+						onSubmit={handleSubmit}
+						title={t('base.store')}
+					>
+						<InputText
+							className='col-md-4'
+							name='store_name'
+							placeholder={t('inputs.store_name')}
+							required
+						/>
+						<InputText
+							className='col-md-4'
+							name='store_owner'
+							placeholder={t('inputs.store_owner')}
+							required
+						/>
+						<InputText
+							className='col-md-4'
+							name='geocode'
+							placeholder={t('inputs.geocode')}
+						/>
+						<InputText
+							className='col-md-4'
+							name='email'
+							placeholder={t('inputs.email')}
+							validationHandler={emailValidate}
+							required
+						/>
+						<InputText
+							className='col-md-4'
+							name='phone'
+							placeholder={t('inputs.phone')}
+							validationHandler={phoneValidate}
+							required
+						/>
+						<InputTextarea name='address' placeholder={t('inputs.address')} />
+						<InputImage name='image' />
+						<InputTextarea
+							name='opening_times'
+							placeholder={t('inputs.opening_times')}
+						/>
+						<InputTextarea name='comment' placeholder={t('inputs.comment')} />
+					</FormLayout>
+				)}
+			</Form>
+		</>
+	);
+};
+
+AdminSettingsStoreForm.displayName = 'AdminSettingsStoreForm';
