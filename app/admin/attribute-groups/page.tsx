14343f73a93e82103eeb8f5b05a9de400ab2531b
Artem <mstrjesm@gmail.com>
2024-07-15 20:56:09 +0300

    feat: add route for attribute-groups

---
+++ app/admin/attribute-groups/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AdminAttributeGroupTable } from '@entities/Admin/ui/attribute';
+
+export default function AttributeGroupPage() {
+	const nodes = [
+		{
+			id: 1,
+			attribute_group_name: 'Memory',
+			sort_order: 2,
+			slug: 'memory',
+		},
+		{
+			id: 2,
+			attribute_group_name: 'Motherboard',
+			sort_order: 3,
+			slug: 'motherboard',
+		},
+		{
+			id: 3,
+			attribute_group_name: 'Processor',
+			sort_order: 4,
+			slug: 'processor',
+		},
+		{
+			id: 4,
+			attribute_group_name: 'Technical',
+			sort_order: 1,
+			slug: 'technical',
+		},
+	];
+
+	return <AdminAttributeGroupTable nodes={nodes} />;
+}
+
+export const metadata = {
+	title: 'Attribute Groups',
+};
