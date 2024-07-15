5e16445eb6171de6f3f8d33fd166b6ef4f7a04f4
Artem <mstrjesm@gmail.com>
2024-07-15 11:34:31 +0300

    refactor(middleware): next-intl config

--- middlware.ts
+++ middlware.ts
@@ -1,10 +1,16 @@
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware({
-  locales: ['en', 'ua'],
-  defaultLocale: 'en'
+const middleware = createMiddleware({
+	// Add locales you want in the app
+	locales: ['en', 'ua'],
+
+	// Default locale if no match
+	defaultLocale: 'en',
 });
 
+export default middleware;
+
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
-}
+	// Match only internationalized pathnames
+	matcher: ['/', '/(ua|en)/:path*'],
+};
