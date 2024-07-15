48cd88f29ad4ed5320778c8bf768c9a93467a596
Artem <mstrjesm@gmail.com>
2024-07-15 11:42:02 +0300

    refactor(icon-user): add react import

--- app/components/shared/icons/IconUser/IconUser.tsx
+++ app/components/shared/icons/IconUser/IconUser.tsx
@@ -1,21 +1,22 @@
-import { FC } from 'react';
+import React, { type FC } from 'react';
 
 export const IconUser: FC = () => {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      fill='none'
-      viewBox='0 0 24 24'>
-      <path
-        fill='currentColor'
-        fill-rule='evenodd'
-        d='M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3-12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 7a7.489 7.489 0 0 1 6-3 7.489 7.489 0 0 1 6 3 7.489 7.489 0 0 1-6 3 7.489 7.489 0 0 1-6-3Z'
-        clip-rule='evenodd'
-      />
-    </svg>
-  );
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			fill='none'
+			viewBox='0 0 24 24'
+		>
+			<path
+				fill='currentColor'
+				fillRule='evenodd'
+				d='M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3-12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 7a7.489 7.489 0 0 1 6-3 7.489 7.489 0 0 1 6 3 7.489 7.489 0 0 1-6 3 7.489 7.489 0 0 1-6-3Z'
+				clipRule='evenodd'
+			/>
+		</svg>
+	);
 };
 
 IconUser.displayName = 'IconUser';
