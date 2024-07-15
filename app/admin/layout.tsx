12c7d7f56defc057fffb40cd248ce71d7420880a
Artem <mstrjesm@gmail.com>
2024-07-15 13:39:23 +0300

    feat(admin-layout): add structure

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import React, { type ReactNode } from 'react';
 import { AdminHeader } from '@entities/Admin/ui/AdminHeader/AdminHeader';
 import { AdminSidebar } from '@entities/Admin/ui/AdminSidebar/AdminSidebar';
+import cls from './AdminLayout.module.scss';
 
 interface LocaleLayoutProps {
 	children: ReactNode;
@@ -8,10 +9,10 @@ interface LocaleLayoutProps {
 
 export default async function AdminLayout({ children }: LocaleLayoutProps) {
 	return (
-		<>
-			<AdminHeader />
-			<AdminSidebar />
-			<main>{children}</main>
-		</>
+		<div className={cls.AdminLayout}>
+			<AdminHeader className={cls.AdminHeader} />
+			<AdminSidebar className={cls.AdminSidebar} />
+			<main className={cls.AdminMain}>{children}</main>
+		</div>
 	);
 }
