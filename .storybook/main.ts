b93d54d879a8eeda3ddb3c5bcf43c69ed4fc72ea
Artem <mstrjesm@gmail.com>
2024-07-15 12:00:12 +0300

    feat(storybook/main): add alias for fonts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,79 +2,80 @@ import { StorybookConfig } from '@storybook/nextjs';
 import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../app/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: ['../app/**/*.stories.@(js|jsx|ts|tsx)'],
 
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-styling-webpack',
-      options: {
-        rules: [
-          {
-            test: /\.(css|s[ac]ss)$/i,
-            use: [
-              'style-loader',
-              'css-loader',
-              {
-                loader: 'sass-loader',
-                options: {
-                  sassOptions: {
-                    implementation: require.resolve('sass'),
-                  },
-                },
-              },
-            ],
-          },
-        ],
-      },
-    },
-    '@chromatic-com/storybook',
-  ],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-onboarding',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+		{
+			name: '@storybook/addon-styling-webpack',
+			options: {
+				rules: [
+					{
+						test: /\.(css|s[ac]ss)$/i,
+						use: [
+							'style-loader',
+							'css-loader',
+							{
+								loader: 'sass-loader',
+								options: {
+									sassOptions: {
+										implementation: require.resolve('sass'),
+									},
+								},
+							},
+						],
+					},
+				],
+			},
+		},
+		'@chromatic-com/storybook',
+	],
 
-  framework: {
-    name: '@storybook/nextjs',
-    options: { builder: {} },
-  },
-  docs: {},
+	framework: {
+		name: '@storybook/nextjs',
+		options: { builder: {} },
+	},
+	docs: {},
 
-  webpackFinal: async (config) => {
-    if (config && config.resolve) {
-      const alias = {
-        '@shared': path.resolve(__dirname, '../app/components/shared'),
-        '@features': path.resolve(__dirname, '../app/components/features'),
-        '@widgets': path.resolve(__dirname, '../app/components/widgets'),
-        '@entities': path.resolve(__dirname, '../app/components/entities'),
-        '@lib': path.resolve(__dirname, '../app/lib'),
-        '@utils': path.resolve(__dirname, '../utils'),
-        '@constants': path.resolve(__dirname, '../constants'),
-        '@styles': path.resolve(__dirname, '../styles'),
-        '@hooks': path.resolve(__dirname, '../hooks'),
-        'next/image': path.resolve(__dirname, './mocks/Image.tsx'),
-      };
+	webpackFinal: async (config) => {
+		if (config?.resolve) {
+			const alias = {
+				'@shared': path.resolve(__dirname, '../app/components/shared'),
+				'@features': path.resolve(__dirname, '../app/components/features'),
+				'@widgets': path.resolve(__dirname, '../app/components/widgets'),
+				'@entities': path.resolve(__dirname, '../app/components/entities'),
+				'@lib': path.resolve(__dirname, '../app/lib'),
+				'@utils': path.resolve(__dirname, '../utils'),
+				'@constants': path.resolve(__dirname, '../constants'),
+				'@styles': path.resolve(__dirname, '../styles'),
+				'@hooks': path.resolve(__dirname, '../hooks'),
+				'/fonts': path.resolve(__dirname, '../public/fonts'),
+				'next/image': path.resolve(__dirname, './mocks/Image.tsx'),
+			};
 
-      config.resolve.alias = { ...config.resolve.alias, ...alias };
-    }
+			config.resolve.alias = { ...config.resolve.alias, ...alias };
+		}
 
-    // Enable caching for faster rebuilds
-    if (config && config.cache) {
-      config.cache = {
-        type: 'filesystem',
-        buildDependencies: {
-          config: [__filename],
-        },
-      };
-    }
+		// Enable caching for faster rebuilds
+		if (config?.cache) {
+			config.cache = {
+				type: 'filesystem',
+				buildDependencies: {
+					config: [__filename],
+				},
+			};
+		}
 
-    return config;
-  },
+		return config;
+	},
 
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-  },
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+	},
 };
 
 export default config;
