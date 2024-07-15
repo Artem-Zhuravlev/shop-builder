305d9e61b782cf001886f56f53670d2bc0a8522d
Artem <mstrjesm@gmail.com>
2024-07-15 11:40:41 +0300

    refactor(admin-sidebar): add use-client and react import

--- app/components/entities/Admin/ui/AdminSidebar/AdminSidebar.tsx
+++ app/components/entities/Admin/ui/AdminSidebar/AdminSidebar.tsx
@@ -1,61 +1,63 @@
+'use client';
 import classNames from 'classnames';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
-import { useState, type FC } from 'react';
+import React, { useState, type FC } from 'react';
 import { Collapse } from 'react-collapse';
 import cls from './AdminSidebar.module.scss';
 import { navbarList, type SidebarItem } from './AdminSidebarList';
 
 const SidebarNavItem: FC<{ item: SidebarItem }> = ({ item }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const t = useTranslations('admin');
+	const [isOpen, setIsOpen] = useState(false);
+	const t = useTranslations('admin');
 
-  return (
-    <li key={item.value}>
-      {item.to ? (
-        <Link href={item.to} className={cls.SidebarItemLink}>
-          {item.icon && <span className={item.icon} />}
-          {t(item.value)}
-        </Link>
-      ) : (
-        <button
-          type='button'
-          onClick={() => setIsOpen(!isOpen)}
-          className={classNames(cls.SidebarItemBtn, {
-            [cls.SidebarItemBtnOpen]: isOpen,
-          })}>
-          {item.icon && <span className={item.icon} />}
-          {t(item.value)}
-        </button>
-      )}
+	return (
+		<li key={item.value}>
+			{item.to ? (
+				<Link href={item.to} className={cls.SidebarItemLink}>
+					{item.icon && <span className={item.icon} />}
+					{t(item.value)}
+				</Link>
+			) : (
+				<button
+					type='button'
+					onClick={() => setIsOpen(!isOpen)}
+					className={classNames(cls.SidebarItemBtn, {
+						[cls.SidebarItemBtnOpen]: isOpen,
+					})}
+				>
+					{item.icon && <span className={item.icon} />}
+					{t(item.value)}
+				</button>
+			)}
 
-      {item.children && (
-        <Collapse isOpened={isOpen}>
-          <SidebarList items={item.children} />
-        </Collapse>
-      )}
-    </li>
-  );
+			{item.children && (
+				<Collapse isOpened={isOpen}>
+					<SidebarList items={item.children} />
+				</Collapse>
+			)}
+		</li>
+	);
 };
 
 const SidebarList: FC<{ items: SidebarItem[] }> = ({ items }) => {
-  return (
-    <ul className={cls.SidebarList}>
-      {items.map((item) => (
-        <SidebarNavItem key={item.value} item={item} />
-      ))}
-    </ul>
-  );
+	return (
+		<ul className={cls.SidebarList}>
+			{items.map((item) => (
+				<SidebarNavItem key={item.value} item={item} />
+			))}
+		</ul>
+	);
 };
 
 export const AdminSidebar: FC = () => {
-  const [navbar] = useState(navbarList);
+	const [navbar] = useState(navbarList);
 
-  return (
-    <aside className={cls.AdminSidebar}>
-      <SidebarList items={navbar} />
-    </aside>
-  );
+	return (
+		<aside className={cls.AdminSidebar}>
+			<SidebarList items={navbar} />
+		</aside>
+	);
 };
 
 AdminSidebar.displayName = 'AdminSidebar';
