2eeaa662309ad9f60e42e125521586be650a2183
Artem <mstrjesm@gmail.com>
2024-07-15 20:18:42 +0300

    feat: add dynamic filters  route

---
+++ app/admin/filters/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AdminFiltersForm } from '@entities/Admin/ui/filters';
+
+export default function CategoriesFormPage({
+	params,
+}: { params: { slug: string } }) {
+	// TODO: add logic if NEW - slug don't send request
+	return <AdminFiltersForm />;
+}
+
+export const metadata = {
+	title: 'Filters',
+};
