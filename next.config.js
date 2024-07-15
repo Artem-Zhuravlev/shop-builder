fb21bde8e1677ccc92ebf8cc7c708a66c5cfb10d
Artem <mstrjesm@gmail.com>
2024-07-15 11:30:16 +0300

    refactor(next.config): add next-intl wrapper

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
+
 /** @type {import('next').NextConfig} */
-
-const path = require('path');
-
+const path = require('node:path');
 const nextConfig = {
 	reactStrictMode: true,
-};
-
-module.exports = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
 	},
 	webpack: (config) => {
 		config.resolve.alias['@'] = path.join(__dirname, 'styles');
+		return config;
 	},
 };
+
+const withNextIntl = require('next-intl/plugin')(
+	'./i18n.ts'
+);
+
+module.exports = withNextIntl(nextConfig);
