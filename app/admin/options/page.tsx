7cda590e89a8ddc65e5d9b94b69b1fe7a564846d
Artem <mstrjesm@gmail.com>
2024-07-16 12:33:48 +0300

    feat: add options route

---
+++ app/admin/options/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AdminOptionsTable } from '@entities/Admin/ui/options';
+
+export default function OptionsPage() {
+	const nodes = [
+		{
+			id: 1,
+			sort_order: 1,
+			option_name: 'Checkbox',
+			slug: 'checkbox',
+		},
+		{
+			id: 2,
+			sort_order: 2,
+			option_name: 'Color',
+			slug: 'color',
+		},
+		{
+			id: 3,
+			sort_order: 4,
+			option_name: 'Date',
+			slug: 'date',
+		},
+		{
+			id: 4,
+			sort_order: 5,
+			option_name: 'Date & Time',
+			slug: 'date-and-time',
+		},
+	];
+
+	return <AdminOptionsTable nodes={nodes} />;
+}
+
+export const metadata = {
+	title: 'Options',
+};
