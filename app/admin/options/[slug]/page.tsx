24b2ebb7ff5188df2c059edc32a865b4dfa2ae04
Artem <mstrjesm@gmail.com>
2024-07-16 12:41:16 +0300

    feat: add dynamic options route

---
+++ app/admin/options/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AdminOptionsForm } from '@entities/Admin/ui/options';
+
+export default function OptionsFormPage({
+	params,
+}: { params: { slug: string } }) {
+	// TODO: add logic if NEW - slug don't send request
+	return <AdminOptionsForm />;
+}
+
+export const metadata = {
+	title: 'Options',
+};
