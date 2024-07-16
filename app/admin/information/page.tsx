b2c9daff81749e58183bde86402c6f6a19a8acea
Artem <mstrjesm@gmail.com>
2024-07-16 14:39:42 +0300

    feat: add information route

---
+++ app/admin/information/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AdminInformationTable } from '@entities/Admin/ui/information';
+
+export default function InformationPage() {
+	const nodes = [
+		{
+			id: 1,
+			title: 'About Us',
+			sort_order: 1,
+			slug: 'about',
+		},
+		{
+			id: 2,
+			title: 'Delivery Information',
+			sort_order: 2,
+			slug: 'delivery',
+		},
+		{
+			id: 3,
+			title: 'Privacy Policy',
+			sort_order: 3,
+			slug: 'privacy',
+		},
+		{
+			id: 4,
+			title: 'Terms & Conditions',
+			sort_order: 4,
+			slug: 'terms',
+		},
+	];
+
+	return <AdminInformationTable nodes={nodes} />;
+}
+
+export const metadata = {
+	title: 'Information',
+};
