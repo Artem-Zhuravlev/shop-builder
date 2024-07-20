ccc6928c1cafa9a58222cdc312b4a3e92c054855
Artem <mstrjesm@gmail.com>
2024-07-20 18:24:54 +0300

    feat(admin-returns-table): add use client, fix next navigation path

--- app/components/entities/Admin/ui/returns/AdminReturnsTable/AdminReturnsTable.tsx
+++ app/components/entities/Admin/ui/returns/AdminReturnsTable/AdminReturnsTable.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { StatusLabel, type Status } from '@features/tables';
 import { useFormattedDate } from '@hooks/useFormattedDate';
 import { ButtonBase } from '@shared/ButtonBase';
@@ -27,10 +28,10 @@ import {
 } from '@table-library/react-table-library/table';
 import { useTheme } from '@table-library/react-table-library/theme';
 import { useTranslations } from 'next-intl';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { type FC } from 'react';
 
-interface AdminReturnsTableItem extends TableNode {
+export interface AdminReturnsTableItem extends TableNode {
 	id: string | number;
 	return_id: number;
 	order_id: number;
