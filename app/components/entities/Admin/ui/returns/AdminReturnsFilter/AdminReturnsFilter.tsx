700345246332bc0f4c6a87e90411f0b7ba5505c6
Artem <mstrjesm@gmail.com>
2024-07-20 18:27:02 +0300

    feat(admin-returns-filter): add use client, add wrapper

--- app/components/entities/Admin/ui/returns/AdminReturnsFilter/AdminReturnsFilter.tsx
+++ app/components/entities/Admin/ui/returns/AdminReturnsFilter/AdminReturnsFilter.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { ButtonBase } from '@shared/ButtonBase';
 import { InputDatePicker, InputText, InputReturnStatus } from '@shared/inputs';
 import { useTranslations } from 'next-intl';
@@ -11,38 +12,40 @@ export const AdminReturnsFilter: FC = () => {
 	};
 
 	return (
-		<Form
-			onSubmit={onSubmit}
-			render={({ handleSubmit }) => {
-				return (
-					<form onSubmit={handleSubmit}>
-						<h3 className='h2'>{t('base.filter')}</h3>
-						<InputText
-							type='number'
-							name='return_id'
-							placeholder={t('inputs.return_id')}
-						/>
-						<InputText
-							type='number'
-							name='order_id'
-							placeholder={t('inputs.order_id')}
-						/>
-						<InputText name='customer' placeholder={t('inputs.customer')} />
-						<InputText name='product' placeholder={t('inputs.product')} />
-						<InputText name='model' placeholder={t('inputs.model')} />
-						<InputReturnStatus name='status' />
-						<InputDatePicker
-							name='date_added'
-							placeholder={t('inputs.date_added')}
-						/>
-						<ButtonBase>
-							<span className='icon-filter' />
-							{t('base.filter')}
-						</ButtonBase>
-					</form>
-				);
-			}}
-		/>
+		<div className='form-holder'>
+			<Form
+				onSubmit={onSubmit}
+				render={({ handleSubmit }) => {
+					return (
+						<form onSubmit={handleSubmit}>
+							<h3 className='h2'>{t('base.filter')}</h3>
+							<InputText
+								type='number'
+								name='return_id'
+								placeholder={t('inputs.return_id')}
+							/>
+							<InputText
+								type='number'
+								name='order_id'
+								placeholder={t('inputs.order_id')}
+							/>
+							<InputText name='customer' placeholder={t('inputs.customer')} />
+							<InputText name='product' placeholder={t('inputs.product')} />
+							<InputText name='model' placeholder={t('inputs.model')} />
+							<InputReturnStatus name='status' />
+							<InputDatePicker
+								name='date_added'
+								placeholder={t('inputs.date_added')}
+							/>
+							<ButtonBase>
+								<span className='icon-filter' />
+								{t('base.filter')}
+							</ButtonBase>
+						</form>
+					);
+				}}
+			/>
+		</div>
 	);
 };
 
