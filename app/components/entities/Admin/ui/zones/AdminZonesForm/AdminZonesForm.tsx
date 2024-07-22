8b7c32db8ba695e573f702cf257da000e843b55c
Artem <mstrjesm@gmail.com>
2024-07-22 18:31:15 +0300

    feat(admin-zones-table): add use client, fix next navigation route

--- app/components/entities/Admin/ui/zones/AdminZonesForm/AdminZonesForm.tsx
+++ app/components/entities/Admin/ui/zones/AdminZonesForm/AdminZonesForm.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { InputCheckbox, InputSelect, InputText } from '@shared/inputs';
 import { FormLayout } from '@widgets/FormLayout';
 import { useTranslations } from 'next-intl';
@@ -12,45 +13,47 @@ export const AdminZonesForm: FC = () => {
 	};
 
 	return (
-		<Form
-			onSubmit={onSubmit}
-			render={({ handleSubmit }) => {
-				return (
-					<FormLayout
-						className='row align-items-center'
-						onSubmit={handleSubmit}
-						title={t('base.zones')}
-					>
-						<InputText
-							className='col-md-4'
-							name='zone_name'
-							placeholder={t('admin.zone_name')}
-							required
-						/>
-						<InputText
-							className='col-md-4'
-							name='zone_code'
-							placeholder={t('admin.zone_code')}
-						/>
-						<InputSelect
-							className='col-md-4'
-							name='country'
-							placeholder={t('inputs.country')}
-							required
-							options={[
-								{ value: 'togo', label: 'Togo' },
-								{ value: 'kenya', label: 'Kenya' },
-							]}
-						/>
-						<InputCheckbox
-							className='col-md-4'
-							name='status'
-							label={t('inputs.status')}
-						/>
-					</FormLayout>
-				);
-			}}
-		/>
+		<div className='form-holder'>
+			<Form
+				onSubmit={onSubmit}
+				render={({ handleSubmit }) => {
+					return (
+						<FormLayout
+							className='row align-items-center'
+							onSubmit={handleSubmit}
+							title={t('base.zones')}
+						>
+							<InputText
+								className='col-md-4'
+								name='zone_name'
+								placeholder={t('admin.zone_name')}
+								required
+							/>
+							<InputText
+								className='col-md-4'
+								name='zone_code'
+								placeholder={t('admin.zone_code')}
+							/>
+							<InputSelect
+								className='col-md-4'
+								name='country'
+								placeholder={t('inputs.country')}
+								required
+								options={[
+									{ value: 'togo', label: 'Togo' },
+									{ value: 'kenya', label: 'Kenya' },
+								]}
+							/>
+							<InputCheckbox
+								className='col-md-4'
+								name='status'
+								label={t('inputs.status')}
+							/>
+						</FormLayout>
+					);
+				}}
+			/>
+		</div>
 	);
 };
 
