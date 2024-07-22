6cc4d26360c8a5c5a2a9d5beb22904b65f20033d
Artem <mstrjesm@gmail.com>
2024-07-22 15:03:23 +0300

    feat(admin-stock-status-form): add use client, add wrapper

--- app/components/entities/Admin/ui/stockStatus/AdminStockStatusForm/AdminStockStatusForm.tsx
+++ app/components/entities/Admin/ui/stockStatus/AdminStockStatusForm/AdminStockStatusForm.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { InputText } from '@shared/inputs';
 import { FormLayout } from '@widgets/FormLayout';
 import { useTranslations } from 'next-intl';
@@ -12,25 +13,27 @@ export const AdminStockStatusForm: FC = () => {
 	};
 
 	return (
-		<Form
-			onSubmit={onSubmit}
-			render={({ handleSubmit }) => {
-				return (
-					<FormLayout
-						className='row align-items-center'
-						onSubmit={handleSubmit}
-						title={t('base.stock_statuses')}
-					>
-						<InputText
-							className='col-md-4'
-							name='stock_status_name'
-							placeholder={t('inputs.stock_status_name')}
-							required
-						/>
-					</FormLayout>
-				);
-			}}
-		/>
+		<div className='form-holder'>
+			<Form
+				onSubmit={onSubmit}
+				render={({ handleSubmit }) => {
+					return (
+						<FormLayout
+							className='row align-items-center'
+							onSubmit={handleSubmit}
+							title={t('base.stock_statuses')}
+						>
+							<InputText
+								className='col-md-4'
+								name='stock_status_name'
+								placeholder={t('inputs.stock_status_name')}
+								required
+							/>
+						</FormLayout>
+					);
+				}}
+			/>
+		</div>
 	);
 };
 
