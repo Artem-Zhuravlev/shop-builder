da0a66afbd444553c2f78785a52003430b179f7a
Artem <mstrjesm@gmail.com>
2024-07-22 15:10:12 +0300

    feat(admin/stock-statuses): add dynamic route

---
+++ app/admin/stock-statuses/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AdminStockStatusForm } from '@entities/Admin/ui/stockStatus';
+
+export default function StockStatusesFormPage({
+	params,
+}: { params: { slug: string } }) {
+	// TODO: add logic if NEW - slug don't send request
+	return <AdminStockStatusForm />;
+}
+
+export const metadata = {
+	title: 'Stock statuses',
+};
