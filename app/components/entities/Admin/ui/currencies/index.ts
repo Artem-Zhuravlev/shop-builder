2550076188ca57b4f8a94f15fe93d56d31168eb9
Artem <mstrjesm@gmail.com>
2024-07-22 14:48:15 +0300

    feat(admin/currencies): add imports

---
+++ app/components/entities/Admin/ui/currencies/index.ts
@@ -0,0 +1,5 @@
+export { AdminCurrencyForm } from './AdminCurrencyForm/AdminCurrencyForm';
+export {
+	AdminCurrencyTable,
+	type AdminCurrencyTableItem,
+} from './AdminCurrencyTable/AdminCurrencyTable';
