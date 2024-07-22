9ccb8b97f58299f18af6c8ce10932a9acda1550b
Artem <mstrjesm@gmail.com>
2024-07-22 18:08:47 +0300

    feat(admin/countries): add imports

---
+++ app/components/entities/Admin/ui/countries/index.ts
@@ -0,0 +1,3 @@
+export { AdminCountriesFilter } from './AdminCountriesFilter/AdminCountriesFilter';
+export { AdminCountriesForm } from './AdminCountriesForm/AdminCountriesForm';
+export { AdminCountriesTable } from './AdminCountriesTable/AdminCountriesTable';
