c414a094b56b377de1fcd6502e6a13227d5711a2
Artem <mstrjesm@gmail.com>
2024-07-22 14:48:59 +0300

    feat(admin-currency-form): add use client, add wrapper

--- app/components/entities/Admin/ui/currencies/AdminCurrencyForm/AdminCurrencyForm.tsx
+++ app/components/entities/Admin/ui/currencies/AdminCurrencyForm/AdminCurrencyForm.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { InputCheckbox, InputText } from '@shared/inputs';
 import { FormLayout } from '@widgets/FormLayout';
 import { useTranslations } from 'next-intl';
@@ -12,53 +13,55 @@ export const AdminCurrencyForm: FC = () => {
 	};
 
 	return (
-		<Form
-			onSubmit={onSubmit}
-			render={({ handleSubmit }) => {
-				return (
-					<FormLayout
-						className='row align-items-center'
-						onSubmit={handleSubmit}
-						title={t('base.currency')}
-					>
-						<InputText
-							className='col-md-4'
-							name='currency_title'
-							placeholder={t('inputs.currency_title')}
-							required
-						/>
-						<InputText
-							className='col-md-4'
-							name='code'
-							placeholder={t('inputs.code')}
-							required
-						/>
-						<InputText
-							className='col-md-4'
-							name='symbol'
-							placeholder={t('inputs.symbol')}
-						/>
-						<InputText
-							type='number'
-							className='col-md-4'
-							name='decimal_places'
-							placeholder={t('inputs.decimal_places')}
-						/>
-						<InputText
-							type='number'
-							className='col-md-4'
-							name='value'
-							placeholder={t('inputs.value')}
-						/>
-						<InputCheckbox
-							className='col-md-4'
-							name='status'
-							label={t('inputs.status')}
-						/>
-					</FormLayout>
-				);
-			}}
-		/>
+		<div className='form-holder'>
+			<Form
+				onSubmit={onSubmit}
+				render={({ handleSubmit }) => {
+					return (
+						<FormLayout
+							className='row align-items-center'
+							onSubmit={handleSubmit}
+							title={t('base.currency')}
+						>
+							<InputText
+								className='col-md-4'
+								name='currency_title'
+								placeholder={t('inputs.currency_title')}
+								required
+							/>
+							<InputText
+								className='col-md-4'
+								name='code'
+								placeholder={t('inputs.code')}
+								required
+							/>
+							<InputText
+								className='col-md-4'
+								name='symbol'
+								placeholder={t('inputs.symbol')}
+							/>
+							<InputText
+								type='number'
+								className='col-md-4'
+								name='decimal_places'
+								placeholder={t('inputs.decimal_places')}
+							/>
+							<InputText
+								type='number'
+								className='col-md-4'
+								name='value'
+								placeholder={t('inputs.value')}
+							/>
+							<InputCheckbox
+								className='col-md-4'
+								name='status'
+								label={t('inputs.status')}
+							/>
+						</FormLayout>
+					);
+				}}
+			/>
+		</div>
 	);
 };
 
