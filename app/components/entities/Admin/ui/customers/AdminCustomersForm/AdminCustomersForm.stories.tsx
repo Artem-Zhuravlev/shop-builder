1be18dde9f7bca0c230f7f9ecaee07a610dbd309
Artem <mstrjesm@gmail.com>
2024-07-23 16:17:12 +0300

    feat(admin-customers-form): add parameters for story

--- app/components/entities/Admin/ui/customers/AdminCustomersForm/AdminCustomersForm.stories.tsx
+++ app/components/entities/Admin/ui/customers/AdminCustomersForm/AdminCustomersForm.stories.tsx
@@ -4,6 +4,11 @@ import { AdminCustomersForm } from './AdminCustomersForm';
 export default {
 	title: 'Entities/Admin/Customers/AdminProductsForm',
 	component: AdminCustomersForm,
+	parameters: {
+		nextjs: {
+			appDirectory: true,
+		},
+	},
 } as Meta<typeof AdminCustomersForm>;
 
 const Template: StoryFn<typeof AdminCustomersForm> = (args) => {
