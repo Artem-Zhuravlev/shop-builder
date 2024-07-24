7499539b638b3dcf54f92fab82ec3507228d4fcf
Artem <mstrjesm@gmail.com>
2024-07-24 16:18:33 +0300

    feat(admin-customers-form): add admin0customers-reward-points-form import

--- app/components/entities/Admin/ui/customers/AdminCustomersForm/common/index.ts
+++ app/components/entities/Admin/ui/customers/AdminCustomersForm/common/index.ts
@@ -1,3 +1,4 @@
 export { AdminCustomersAddressesForm } from './AdminCustomersAddressesForm';
 export { AdminCustomersGeneralForm } from './AdminCustomersGeneralForm';
 export { AdminCustomersHistoryForm } from './AdminCustomersHistoryForm';
+export { AdminCustomersRewardPointsForm } from './AdminCustomersRewardPointsForm';
