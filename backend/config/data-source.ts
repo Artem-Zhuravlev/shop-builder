1b9531445c83a9704a94b9865cc198f90aa17aa1
Artem <mstrjesm@gmail.com>
2024-07-30 15:36:20 +0300

    feat(data-source): add DataSource typeorm config

---
+++ backend/config/data-source.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { DataSource, type DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { Settings } from '../entities/Settings';
+import 'dotenv/config';
+
+const config = {
+	type: 'postgres',
+	host: process.env.DATABASE_HOST,
+	port: 5432,
+	username: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME,
+	synchronize: false,
+	namingStrategy: new SnakeNamingStrategy(),
+	logging: false,
+	entities: [Settings],
+};
+
+export const AppDataSource = new DataSource({
+	...config,
+	migrations: ['backend/database/migrations/*'],
+} as DataSourceOptions);
+
+const dataSource = new DataSource(config as DataSourceOptions);
+
+export const getDatabaseConnection = async () => {
+	if (!dataSource.isInitialized) {
+		await dataSource.initialize();
+	}
+	return dataSource;
+};
