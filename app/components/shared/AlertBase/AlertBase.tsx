4b4903dc66f7d35de9689f3612a13b6d44f07baa
Artem <mstrjesm@gmail.com>
2024-08-15 14:52:46 +0300

    refactor(alert-base): add useEffect for props

--- app/components/shared/AlertBase/AlertBase.tsx
+++ app/components/shared/AlertBase/AlertBase.tsx
@@ -1,16 +1,20 @@
 'use client';
-import React, { useState, type FC } from 'react';
+import React, { useEffect, useState, type FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import cls from './AlertBase.module.scss';
-import { AlertItem, type AlertItemProps } from './common/AlertItem';
+import { AlertItem, type AlertItemInterface } from './common/AlertItem';
 
 export interface AlertsProps {
-	alerts: Pick<AlertItemProps, 'type' | 'message'>[];
+	alerts: AlertItemInterface[];
 }
 
 export const AlertBase: FC<AlertsProps> = (props) => {
 	const { alerts } = props;
-	const [alertItems, setAlertItems] = useState(alerts);
+	const [alertItems, setAlertItems] = useState<AlertItemInterface[]>(alerts);
+
+	useEffect(() => {
+		setAlertItems(alerts);
+	}, [alerts]);
 
 	const handleClose = (index: number) => {
 		const updatedAlerts = alertItems.filter((_, i) => i !== index);
