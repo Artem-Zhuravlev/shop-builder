883ddae29b873e28897e5b6930f8d2d0426a2f72
Artem <mstrjesm@gmail.com>
2024-08-25 18:14:40 +0300

    fix: remove unused slice

--- app/lib/redux/rootReducer.ts
+++ app/lib/redux/rootReducer.ts
@@ -1,8 +1,6 @@
 /* Instruments */
-import { counterSlice } from './slices';
 import { productSlice } from './slices';
 
 export const reducer = {
-	counter: counterSlice.reducer,
 	product: productSlice.reducer,
 };
