bf3737d52b610e753139fbf32cf27046f8ef683e
Artem <mstrjesm@gmail.com>
2024-08-27 15:39:25 +0300

    feat(api/settings): add GET endpoint

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -1,3 +1,5 @@
+import { getSettings } from 'backend/services/settings/getSettings';
+import type { SettingsInterface } from 'backend/services/settings/interfaces/settings.interface';
 import { updateSettings } from 'backend/services/settings/updateSettings';
 import { type NextRequest, NextResponse } from 'next/server';
 
@@ -19,6 +21,46 @@ export async function PUT(req: NextRequest) {
 			error instanceof Error ? error.message : 'Unknown error occurred';
 		return NextResponse.json(
 			{ message: 'Error updating settings', error: errorMessage },
+			{ status: 500 },
+		);
+	}
+}
+
+export async function GET(req: NextRequest) {
+	try {
+		const url = new URL(req.url);
+		const idParam = url.searchParams.get('id');
+
+		let settingsInput: SettingsInterface | undefined;
+
+		if (idParam) {
+			const id = Number.parseInt(idParam, 10);
+
+			if (Number.isNaN(id)) {
+				return NextResponse.json(
+					{ message: 'Invalid Settings ID' },
+					{ status: 400 },
+				);
+			}
+
+			settingsInput = { id };
+		}
+
+		const settings = await getSettings(settingsInput);
+
+		if (settings.length === 0) {
+			return NextResponse.json(
+				{ message: 'Settings not found' },
+				{ status: 404 },
+			);
+		}
+
+		return NextResponse.json(settings, { status: 200 });
+	} catch (error) {
+		const errorMessage =
+			error instanceof Error ? error.message : 'Unknown error occurred';
+		return NextResponse.json(
+			{ message: 'Error fetching settings', error: errorMessage },
 			{ status: 500 },
 		);
 	}
