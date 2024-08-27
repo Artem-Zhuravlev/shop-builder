2ab587903fcf1ae55cbbc19c629c92eeb4f64a91
Artem <mstrjesm@gmail.com>
2024-08-27 14:29:22 +0300

    fix(intl-wrapper): replace provider

--- utils/intlWrapper/IntlWrapper.tsx
+++ utils/intlWrapper/IntlWrapper.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { render, type RenderResult } from '@testing-library/react';
 import type { ReactElement } from 'react';
 import { Providers } from '@lib/providers';
-import { NextIntlProvider } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import en from '../../messages/en';
 
 export const customRender = (ui: ReactElement): RenderResult => {
 	return render(
 		<Providers>
-			<NextIntlProvider locale='en' messages={en}>
+			<NextIntlClientProvider locale='en' messages={en}>
 				{ui}
-			</NextIntlProvider>
+			</NextIntlClientProvider>
 		</Providers>,
 	);
 };
