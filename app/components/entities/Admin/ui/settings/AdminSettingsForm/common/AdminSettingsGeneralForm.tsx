76c5c4a8bf8b9f510a1945a0ba0d12d50b3baea7
Artem <mstrjesm@gmail.com>
2024-08-27 15:10:57 +0300

    refactor(admin-settings-general-form): remove Form

--- app/components/entities/Admin/ui/settings/AdminSettingsForm/common/AdminSettingsGeneralForm.tsx
+++ app/components/entities/Admin/ui/settings/AdminSettingsForm/common/AdminSettingsGeneralForm.tsx
@@ -1,44 +1,28 @@
 'use client';
 import { InputImage, InputText, InputTextarea } from '@shared/inputs';
-import { FormLayout } from '@widgets/FormLayout';
 import { useTranslations } from 'next-intl';
 import React, { type FC } from 'react';
-import { Form } from 'react-final-form';
 
 export const AdminSettingsGeneralForm: FC = () => {
 	const t = useTranslations();
 
-	const onSubmit = (values: any) => {
-		console.log('Form Submitted:', values);
-	};
-
 	return (
-		<>
-			<Form onSubmit={onSubmit}>
-				{({ handleSubmit }) => (
-					<FormLayout
-						className='row align-items-center'
-						onSubmit={handleSubmit}
-						title={t('base.general')}
-					>
-						<InputText
-							name='meta_title'
-							placeholder={t('inputs.meta_title')}
-							required
-						/>
-						<InputTextarea
-							name='meta_description'
-							placeholder={t('inputs.meta_description')}
-						/>
-						<InputTextarea
-							name='meta_keywords'
-							placeholder={t('inputs.meta_keywords')}
-						/>
-						<InputImage name='logo' />
-					</FormLayout>
-				)}
-			</Form>
-		</>
+		<div className='row align-items-center'>
+			<InputText
+				name='meta_title'
+				placeholder={t('inputs.meta_title')}
+				required
+			/>
+			<InputTextarea
+				name='meta_description'
+				placeholder={t('inputs.meta_description')}
+			/>
+			<InputTextarea
+				name='meta_keywords'
+				placeholder={t('inputs.meta_keywords')}
+			/>
+			<InputImage name='logo' />
+		</div>
 	);
 };
 
