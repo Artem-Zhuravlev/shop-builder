6b71a7765ea3ffebecd1c5b5ddc029514f131de8
Artem <mstrjesm@gmail.com>
2024-08-30 12:40:58 +0300

    refactor(admin/settings): route

---
+++ app/admin/settings/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AdminSettingsForm } from '@entities/Admin/ui/settings';
+import { getApiSettingsById } from '@shared/api/admin';
+
+export default async function SettingsFormPage({
+	params,
+}: { params: { id: number | string } }) {
+	console.log(params, 'params');
+
+	const initialValues =
+		params.id !== 'new' ? await getApiSettingsById(Number(params.id)) : null;
+
+	// TODO: add logic if NEW - slug don't send request
+	return <AdminSettingsForm id={params.id} initialValues={initialValues} />;
+}
+
+export const metadata = {
+	title: 'Settings',
+};
