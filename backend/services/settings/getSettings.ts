f554289c6e82c87c2b4b9496ead663c0efc3760b
Artem <mstrjesm@gmail.com>
2024-09-01 13:03:33 +0300

    refactor(getSettings): replace interface name

--- backend/services/settings/getSettings.ts
+++ backend/services/settings/getSettings.ts
@@ -1,9 +1,9 @@
 import { getDatabaseConnection } from '../../config/data-source';
 import { Settings } from '../../entities/Settings';
-import type { SettingsInterface } from './interfaces/settings.interface';
+import type { RequestSettingsInterface } from './interfaces/settings.interface';
 
 export const getSettings = async (
-	data?: SettingsInterface,
+	data?: RequestSettingsInterface,
 ): Promise<Settings[]> => {
 	const connection = await getDatabaseConnection();
 
