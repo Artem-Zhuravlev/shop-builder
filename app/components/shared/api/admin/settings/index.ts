9a985218c6b5347b4ac87fed1742ff5cba7b53a9
Artem <mstrjesm@gmail.com>
2024-09-01 12:59:45 +0300

    feat: add public api for endpoints

---
+++ app/components/shared/api/admin/settings/index.ts
@@ -0,0 +1,4 @@
+export { getApiSettings } from './getApiSettings';
+export { getApiSettingsById } from './getApiSettingsById';
+export { updateApiSettings } from './updateApiSettings';
+export { createApiSettings } from './createApiSettings';
