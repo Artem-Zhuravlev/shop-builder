ab1f2a8a09a257f51140a86d0cf815eda3ec2407
Artem <mstrjesm@gmail.com>
2024-09-01 13:05:59 +0300

    feat(getApiSettins): add endpoint

---
+++ app/components/shared/api/admin/settings/getApiSettings.ts
@@ -0,0 +1,22 @@
+import type { AdminSettingsTableItem } from '@entities/Admin/ui/settings';
+import { fetchApi } from '@utils/fetchApi';
+
+/**
+ * Fetches the application settings from the API.
+ *
+ * @returns {Promise<any>} - A promise that resolves with the settings data.
+ * @throws {Error} - Throws an error if the fetch operation fails or the response is not ok.
+ */
+export const getApiSettings = async (): Promise<AdminSettingsTableItem[]> => {
+	try {
+		const response: AdminSettingsTableItem[] = await fetchApi('/settings', {
+			method: 'GET',
+		});
+		return response;
+	} catch (error) {
+		if (error instanceof Error) {
+			throw new Error(`Failed to fetch settings: ${error.message}`);
+		}
+		throw new Error('An unknown error occurred while fetching settings.');
+	}
+};
