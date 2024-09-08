10dc228519aaa824d9522a677b27ff977ab318db
Artem <mstrjesm@gmail.com>
2024-09-08 17:57:25 +0300

    feat(admin-table-navbar): add component

---
+++ src/components/entities/Admin/ui/AdminTableNavbar/AdminTableNavbar.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState, type FC } from 'react';
+import cls from './AdminTableNavbar.module.scss';
+import { useTranslations } from 'next-intl';
+import { ButtonBase } from '@/components/shared/ButtonBase';
+import { useRouter } from 'next/navigation';
+
+interface AdminTableNavbarProps {
+	id?: number;
+	title: string;
+	route: string;
+	onDelete: (id: number) => void;
+}
+
+export const AdminTableNavbar: FC<AdminTableNavbarProps> = ({
+	id,
+	title,
+	route,
+	onDelete,
+}) => {
+	const [showDeleteBtn, setShowDeleteBtn] = useState(false);
+	const t = useTranslations('base');
+	const router = useRouter();
+
+	useEffect(() => {
+		setShowDeleteBtn(id !== undefined);
+	}, [id]);
+
+	const handleDelete = () => {
+		if (id !== undefined) {
+			onDelete(id);
+		}
+	};
+
+	return (
+		<nav className={cls.AdminTableNavbar}>
+			<h3 className='mb-0'>{title}</h3>
+			<ul className={cls.AdminTableNavbarList}>
+				<li className={cls.AdminTableNavbarItem}>
+					<ButtonBase
+						variant='success'
+						onClick={() => router.push(`${route}/new`)}
+					>
+						{t('add_new')}
+					</ButtonBase>
+				</li>
+				{showDeleteBtn && (
+					<li className={cls.AdminTableNavbarItem}>
+						<ButtonBase
+							variant='danger'
+							onClick={handleDelete}
+							data-testid='delete'
+						>
+							<span className='icon-bin' />
+						</ButtonBase>
+					</li>
+				)}
+			</ul>
+		</nav>
+	);
+};
