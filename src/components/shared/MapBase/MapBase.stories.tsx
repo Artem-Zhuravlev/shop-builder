78e683b7c42b69ef0e8ca8ea74216af483287b46
Artem <mstrjesm@gmail.com>
2024-09-09 18:08:13 +0300

    refactor(map-base): add import types

--- src/components/shared/MapBase/MapBase.stories.tsx
+++ src/components/shared/MapBase/MapBase.stories.tsx
@@ -1,19 +1,19 @@
-import { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 import { MapBase } from './MapBase';
 
 export default {
-  title: 'Shared/MapBase',
-  component: MapBase,
+	title: 'Shared/MapBase',
+	component: MapBase,
 } as Meta<typeof MapBase>;
 
 const Template: StoryFn<typeof MapBase> = (args) => <MapBase {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  center: [49.4647755, 36.8515745],
-  position: [49.4647755, 36.8515745],
-  zoom: 13,
-  scrollWheelZoom: false,
-  height: '500px',
-  popupText: 'A pretty CSS3 popup. <br /> Easily customizable.',
+	center: [49.4647755, 36.8515745],
+	position: [49.4647755, 36.8515745],
+	zoom: 13,
+	scrollWheelZoom: false,
+	height: '500px',
+	popupText: 'A pretty CSS3 popup. <br /> Easily customizable.',
 };
