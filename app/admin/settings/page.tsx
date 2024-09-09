d46040241e8e42cf089328064f75ce6938a50c56
Artem <mstrjesm@gmail.com>
2024-09-09 18:11:42 +0300

    refactor(admin/settings): page

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import { AdminTableLayout } from '@/components/widgets/AdminTableLayout';
-import {
-	AdminSettingsTable,
-	type AdminSettingsTableItem,
-} from '@/components/entities/Admin/ui/settings';
-import { PaginateBase } from '@/components/shared/PaginateBase';
-import { getApiSettings } from '@/components/shared/api/admin';
+import { AdminSettingsTableView } from '@/components/entities/Admin/ui/settings';
 
 export default async function SettingsPage() {
-	const nodes: AdminSettingsTableItem[] = await getApiSettings();
-
-	return (
-		<AdminTableLayout>
-			<AdminSettingsTable nodes={nodes} />
-			<PaginateBase pageCount={5} />
-		</AdminTableLayout>
-	);
+	return <AdminSettingsTableView />;
 }
 
 export const metadata = {
