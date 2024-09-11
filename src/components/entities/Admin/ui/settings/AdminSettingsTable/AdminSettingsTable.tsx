d2ce31a96f51b83e387960be1e4d0e58b26b60c7
Artem <mstrjesm@gmail.com>
2024-09-11 20:34:12 +0300

    feat(admin-settings-table): add empty-data-table-row

--- src/components/entities/Admin/ui/settings/AdminSettingsTable/AdminSettingsTable.tsx
+++ src/components/entities/Admin/ui/settings/AdminSettingsTable/AdminSettingsTable.tsx
@@ -22,19 +22,26 @@ import { useTheme } from '@table-library/react-table-library/theme';
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
 import React, { type FC } from 'react';
+import type { SettingsInterface } from '@interfaces/settings';
+import type {
+	Action,
+	State,
+} from '@table-library/react-table-library/types/common';
+import { EmptyDataTableRow } from '@/components/features/tables';
 
 export interface AdminSettingsTableItem extends TableNode {
 	id: string | number;
 	store_name: string;
-	store_url: string;
+	store_owner: string;
 }
 
 interface AdminSettingsTableProps {
-	nodes: AdminSettingsTableItem[];
+	nodes: SettingsInterface[];
+	onSelect: (action: Action) => void;
 }
 
 export const AdminSettingsTable: FC<AdminSettingsTableProps> = (props) => {
-	const { nodes } = props;
+	const { nodes, onSelect } = props;
 	const data = { nodes };
 	const t = useTranslations('admin');
 	const router = useRouter();
@@ -48,40 +55,44 @@ export const AdminSettingsTable: FC<AdminSettingsTableProps> = (props) => {
 		onChange: onSelectChange,
 	});
 
-	function onSelectChange(action: any, state: any) {
-		console.log(action, state);
+	function onSelectChange(action: Action, state: State) {
+		onSelect(action);
 	}
 
 	return (
 		<Table data={data} theme={theme} layout={{ custom: true }} select={select}>
-			{(tableList: AdminSettingsTableItem[]) => (
+			{(tableList: SettingsInterface[]) => (
 				<>
 					<Header>
 						<HeaderRow>
 							<HeaderCellSelect />
 							<HeaderCell>{t('store_name')}</HeaderCell>
-							<HeaderCell>{t('store_url')}</HeaderCell>
+							<HeaderCell>{t('store_owner')}</HeaderCell>
 							<HeaderCell>{t('action')}</HeaderCell>
 						</HeaderRow>
 					</Header>
 
 					<Body>
-						{tableList.map((item) => (
-							<Row item={item} key={item.id}>
-								<CellSelect item={item} />
-								<Cell>{item.store_name}</Cell>
-								<Cell>{item.store_url}</Cell>
-								<Cell>
-									<ButtonBase
-										onClick={() =>
-											router.push(`${ADMIN_ROUTES.settings}/${item.id}`)
-										}
-									>
-										<span className='icon-pencil' />
-									</ButtonBase>
-								</Cell>
-							</Row>
-						))}
+						{tableList.length > 0 ? (
+							tableList.map((item) => (
+								<Row item={item} key={item.id}>
+									<CellSelect item={item} />
+									<Cell>{item.store_name}</Cell>
+									<Cell>{item.store_owner}</Cell>
+									<Cell>
+										<ButtonBase
+											onClick={() =>
+												router.push(`${ADMIN_ROUTES.settings}/${item.id}`)
+											}
+										>
+											<span className='icon-pencil' />
+										</ButtonBase>
+									</Cell>
+								</Row>
+							))
+						) : (
+							<EmptyDataTableRow cells={4} />
+						)}
 					</Body>
 				</>
 			)}
