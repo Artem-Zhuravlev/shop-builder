4a2017939ab5bef7c5f292a8a49d0fae4b9d8f35
Artem <mstrjesm@gmail.com>
2024-09-19 16:39:05 +0300

    fix(request-settings-interface): remove interface

--- backend/services/settings/interfaces/settings.interface.ts
+++
@@ -1,7 +0,0 @@
-import type { SettingsInterface } from '@interfaces/settings';
-import type { FindOperator } from 'typeorm';
-
-export interface RequestSettingsInterface
-	extends Omit<SettingsInterface, 'id'> {
-	id: number | FindOperator<number> | undefined;
-}
