f7c252ab0dd9716a18ba73b7fff5cefd753c354c
Artem <mstrjesm@gmail.com>
2024-09-19 16:57:28 +0300

    fix(api/settings): remove unused imports

--- src/components/shared/api/admin/settings/index.ts
+++ src/components/shared/api/admin/settings/index.ts
@@ -1,4 +1,2 @@
 export { getApiSettings } from './getApiSettings';
-export { getApiSettingsById } from './getApiSettingsById';
 export { updateApiSettings } from './updateApiSettings';
-export { createApiSettings } from './createApiSettings';
