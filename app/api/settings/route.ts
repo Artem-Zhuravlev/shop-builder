b647aca083f9ed2c10b8c30276b1a617a007624b
Artem <mstrjesm@gmail.com>
2024-09-19 16:51:14 +0300

    fix(api/settins): routes

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -1,11 +1,10 @@
-import { createSettings, getSettings } from 'backend/services/settings';
+import { getSettings, updateSettings } from 'backend/services/settings';
 import type { SettingsInterface } from '@interfaces/settings';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
 	try {
-		let settingsInput: SettingsInterface | undefined;
-		const settings = await getSettings(settingsInput);
+		const settings = await getSettings();
 
 		return NextResponse.json(settings, { status: 200 });
 	} catch (error) {
@@ -18,10 +17,10 @@ export async function GET() {
 	}
 }
 
-export async function POST(request: NextRequest) {
+export async function PUT(request: NextRequest) {
 	try {
 		const data: SettingsInterface = await request.json();
-		const createdSettings = await createSettings(data);
+		const createdSettings = await updateSettings(data);
 
 		return NextResponse.json(createdSettings, { status: 200 });
 	} catch (error) {
