f2f72c6f546decd0dc14d16873c9cff9a428ab29
Artem <mstrjesm@gmail.com>
2024-09-19 16:47:49 +0300

    fix(admin/settings): remove imports

--- src/components/entities/Admin/ui/settings/index.ts
+++ src/components/entities/Admin/ui/settings/index.ts
@@ -1,6 +1,1 @@
 export { AdminSettingsForm } from './AdminSettingsForm/AdminSettingsForm';
-export {
-	AdminSettingsTable,
-	type AdminSettingsTableItem,
-} from './AdminSettingsTable/AdminSettingsTable';
-export { AdminSettingsTableView } from './AdminSettingsTableView/AdminSettingsTableView';
