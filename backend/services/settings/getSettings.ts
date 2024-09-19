acdd9eb408c8d91b4348d610d0b25953861e659a
Artem <mstrjesm@gmail.com>
2024-09-19 17:00:23 +0300

    refactor(get-settings): service logic

--- backend/services/settings/getSettings.ts
+++ backend/services/settings/getSettings.ts
@@ -1,39 +1,15 @@
 import { getDatabaseConnection } from '../../config/data-source';
 import { Settings } from '../../entities/Settings';
-import type { RequestSettingsInterface } from './interfaces/settings.interface';
+import type { SettingsInterface } from '@interfaces/settings';
 
-export const getSettings = async (
-	data?: RequestSettingsInterface,
-	id?: number,
-): Promise<Settings[]> => {
+export const getSettings = async (): Promise<object | null> => {
 	const connection = await getDatabaseConnection();
 
-	let settingsRecords: Settings[];
+	const settingsRecords = await connection.manager.find(Settings);
 
-	if (data?.id) {
-		const { id } = data;
-		const settingsRecord = await connection.manager.findOne(Settings, {
-			where: { id },
-		});
-		settingsRecords = settingsRecord ? [settingsRecord] : [];
-	} else {
-		settingsRecords = await connection.manager.find(Settings);
+	if (settingsRecords.length > 0) {
+		return settingsRecords[0].data as SettingsInterface;
 	}
 
-	return settingsRecords;
-};
-
-export const getByIdSettings = async (id: number): Promise<Settings | null> => {
-	const connection = await getDatabaseConnection();
-
-	try {
-		const settingsRecord = await connection.manager.findOne(Settings, {
-			where: { id },
-		});
-
-		return settingsRecord ?? null;
-	} catch (error) {
-		console.error('Error fetching settings by ID:', error);
-		return null;
-	}
+	return null;
 };
