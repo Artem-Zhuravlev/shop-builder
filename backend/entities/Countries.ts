7d6884d39b44c06a46b60ea32a4aecbf9e04e83c
Artem <mstrjesm@gmail.com>
2024-09-22 14:55:25 +0300

    feat(entities/countries): add entity

---
+++ backend/entities/Countries.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'countries' })
+export class Countries {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column({
+		name: 'country',
+		type: 'varchar',
+		nullable: true,
+	})
+	country?: string | null;
+
+	@Column({
+		name: 'iso_code_2',
+		type: 'varchar',
+		length: 2,
+		nullable: true,
+	})
+	iso_code_2!: string;
+
+	@Column({
+		name: 'iso_code_3',
+		type: 'varchar',
+		length: 3,
+		nullable: false,
+	})
+	iso_code_3!: true;
+
+	@Column({
+		name: 'postcode_required',
+		type: 'boolean',
+		nullable: true,
+		default: false,
+	})
+	postcode_required?: boolean | null;
+
+	@Column({
+		name: 'status',
+		type: 'boolean',
+		nullable: true,
+		default: true,
+	})
+	status?: boolean | null;
+}
