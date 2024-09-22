4d13fea7e2eb6baea6de297a2019b3de384bcd8e
Artem <mstrjesm@gmail.com>
2024-09-22 17:04:18 +0300

    feat(admin-countries-form): add alerts

--- src/components/entities/Admin/ui/countries/AdminCountriesForm/AdminCountriesForm.tsx
+++ src/components/entities/Admin/ui/countries/AdminCountriesForm/AdminCountriesForm.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { ErrorAlert, SuccessAlert } from '@/components/features/alerts';
 import { InputCheckbox, InputText } from '@/components/shared/inputs';
 import { FormLayout } from '@/components/widgets/FormLayout';
 import { isoCode2Validate, isoCode3Validate } from '@/utils/validations';
@@ -17,37 +18,41 @@ export const AdminCountriesForm: FC = () => {
 		<div className='form-holder'>
 			<Form
 				onSubmit={onSubmit}
-				render={({ handleSubmit }) => {
+				render={({ handleSubmit, submitFailed, submitSucceeded }) => {
 					return (
-						<FormLayout
-							className='row align-items-center'
-							onSubmit={handleSubmit}
-							title={t('base.countries')}
-						>
-							<InputText
-								className='col-md-4'
-								name='country_name'
-								placeholder={t('admin.country_name')}
-								required
-							/>
-							<InputText
-								className='col-md-4'
-								name='iso_code_2'
-								placeholder={t('admin.iso_code_2')}
-								validationHandler={isoCode2Validate}
-							/>
-							<InputText
-								className='col-md-4'
-								name='iso_code_3'
-								placeholder={t('admin.iso_code_3')}
-								validationHandler={isoCode3Validate}
-							/>
-							<InputCheckbox
-								name='postcode_required'
-								label={t('admin.postcode_required')}
-							/>
-							<InputCheckbox name='status' label={t('inputs.status')} />
-						</FormLayout>
+						<>
+							<FormLayout
+								className='row align-items-center'
+								onSubmit={handleSubmit}
+								title={t('base.countries')}
+							>
+								<InputText
+									className='col-md-4'
+									name='country_name'
+									placeholder={t('admin.country_name')}
+									required
+								/>
+								<InputText
+									className='col-md-4'
+									name='iso_code_2'
+									placeholder={t('admin.iso_code_2')}
+									validationHandler={isoCode2Validate}
+								/>
+								<InputText
+									className='col-md-4'
+									name='iso_code_3'
+									placeholder={t('admin.iso_code_3')}
+									validationHandler={isoCode3Validate}
+								/>
+								<InputCheckbox
+									name='postcode_required'
+									label={t('admin.postcode_required')}
+								/>
+								<InputCheckbox name='status' label={t('inputs.status')} />
+							</FormLayout>
+							<ErrorAlert submitFailed={submitFailed} />
+							<SuccessAlert submitSucceeded={submitSucceeded} />
+						</>
 					);
 				}}
 			/>
