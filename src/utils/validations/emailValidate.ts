19bd28bea4942a29f0f74019411d4d91ef935d0f
Artem <mstrjesm@gmail.com>
2024-09-22 15:43:09 +0300

    refactor(emailValidate): types

--- src/utils/validations/emailValidate.ts
+++ src/utils/validations/emailValidate.ts
@@ -1,10 +1,10 @@
-import { TranslateFunction } from './types/TranslateFunction';
-import { ValidationHandler } from './types/ValidationHandler';
+import type { TranslateFunction } from './types/TranslateFunction';
+import type { ValidationHandler } from './types/ValidationHandler';
 
 export const emailValidate: ValidationHandler = (
 	value: string,
-	t: TranslateFunction
-): string | void => {
+	t: TranslateFunction,
+): string | undefined => {
 	const emailRegex = /^\S+@\S+\.\S+$/;
 	if (!emailRegex.test(value)) {
 		return t('field_error.email_not_valid');
