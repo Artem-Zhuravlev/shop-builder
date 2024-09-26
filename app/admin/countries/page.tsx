960e3ba8ab887f4139345ff6a32306505e7972a9
Artem <mstrjesm@gmail.com>
2024-09-26 17:51:31 +0300

    refactor(admin/countries): to admin-countries-layout

--- app/admin/countries/page.tsx
+++ app/admin/countries/page.tsx
@@ -1,56 +1,8 @@
 import React from 'react';
-import { AdminTableLayout } from '@/components/widgets/AdminTableLayout';
-import {
-	AdminCountriesTable,
-	AdminCountriesFilter,
-} from '@/components/entities/Admin';
-import { PaginateBase } from '@/components/shared/PaginateBase';
+import { AdminCountriesLayout } from '@/components/entities/Admin/ui/countries';
 
-export default function ReturnsPage() {
-	const nodes = [
-		{
-			id: 1,
-			country_name: 'United States',
-			iso_code_2: 'US',
-			iso_code_3: 'USA',
-			slug: 'united-states',
-		},
-		{
-			id: 2,
-			country_name: 'Canada',
-			iso_code_2: 'CA',
-			iso_code_3: 'CAN',
-			slug: 'canada',
-		},
-		{
-			id: 3,
-			country_name: 'United Kingdom',
-			iso_code_2: 'GB',
-			iso_code_3: 'GBR',
-			slug: 'united-kingdom',
-		},
-		{
-			id: 4,
-			country_name: 'Australia',
-			iso_code_2: 'AU',
-			iso_code_3: 'AUS',
-			slug: 'australia',
-		},
-		{
-			id: 5,
-			country_name: 'New Zealand',
-			iso_code_2: 'NZ',
-			iso_code_3: 'NZL',
-			slug: 'new-zealand',
-		},
-	];
-
-	return (
-		<AdminTableLayout filter={<AdminCountriesFilter />}>
-			<AdminCountriesTable nodes={nodes} />
-			<PaginateBase pageCount={5} />
-		</AdminTableLayout>
-	);
+export default function CountriesPage() {
+	return <AdminCountriesLayout />;
 }
 
 export const metadata = {
