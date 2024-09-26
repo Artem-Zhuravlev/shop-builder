5f79108eb6effb5be8f0352163dcd90fe85bb021
Artem <mstrjesm@gmail.com>
2024-09-26 18:06:11 +0300

    feat(api/countries): add GET endpoint

--- app/api/countries/route.ts
+++ app/api/countries/route.ts
@@ -1,5 +1,6 @@
+import { getPaginationParams } from '@/utils/getPaginationParams';
 import type { CountriesInterface } from '@interfaces/index';
-import { createCountry } from 'backend/services/countries';
+import { createCountry, getCountries } from 'backend/services/countries';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
@@ -14,6 +15,23 @@ export async function POST(request: NextRequest) {
 			error instanceof Error ? error.message : 'Unknown error occurred';
 		return NextResponse.json(
 			{ message: 'Error creating country', error: errorMessage },
+			{ status: 500 },
+		);
+	}
+}
+
+export async function GET(request: NextRequest) {
+	try {
+		const { offset, limit } = getPaginationParams(request);
+
+		const countries = await getCountries(offset, limit);
+
+		return NextResponse.json(countries, { status: 200 });
+	} catch (error) {
+		const errorMessage =
+			error instanceof Error ? error.message : 'Unknown error occurred';
+		return NextResponse.json(
+			{ message: 'Error fetching countries', error: errorMessage },
 			{ status: 500 },
 		);
 	}
