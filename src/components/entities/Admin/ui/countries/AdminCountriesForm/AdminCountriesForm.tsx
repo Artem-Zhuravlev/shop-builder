7e2ec736f66504a487f592f548f0e91c8bf20fb0
Artem <mstrjesm@gmail.com>
2024-09-28 15:22:45 +0300

    feat(AdminCountriesForm): add GET PUT endpoints

--- src/components/entities/Admin/ui/countries/AdminCountriesForm/AdminCountriesForm.tsx
+++ src/components/entities/Admin/ui/countries/AdminCountriesForm/AdminCountriesForm.tsx
@@ -1,28 +1,55 @@
 'use client';
-import { ErrorAlert, SuccessAlert } from '@/components/features/alerts';
+import { ErrorAlert } from '@/components/features/alerts';
 import type { CountriesInterface } from '@interfaces/index';
-import { createApiCountry } from '@/components/shared/api/admin';
+import {
+	createApiCountry,
+	updateApiCountry,
+} from '@/components/shared/api/admin';
 import { InputCheckbox, InputText } from '@/components/shared/inputs';
 import { FormLayout } from '@/components/widgets/FormLayout';
 import { isoCode2Validate, isoCode3Validate } from '@/utils/validations';
 import { useTranslations } from 'next-intl';
-import React, { type FC } from 'react';
+import React, { useEffect, useState, type FC } from 'react';
 import { Form } from 'react-final-form';
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
+import { fetchPreselectedForm, handleFormAction } from '@/utils/form';
+import { getApiCountry } from '@/components/shared/api/admin/countries/getApiCountry';
 
 export const AdminCountriesForm: FC = () => {
 	const t = useTranslations();
 	const router = useRouter();
+	const { slug } = useParams();
+	const [initialValues, setInitialValues] = useState<
+		CountriesInterface | undefined
+	>(undefined);
+
+	useEffect(() => {
+		const loadData = async () => {
+			const formData = await fetchPreselectedForm<CountriesInterface>(
+				slug,
+				getApiCountry,
+			);
+			setInitialValues(formData);
+		};
+
+		loadData();
+	}, [slug]);
 
 	const onSubmit = async (value: CountriesInterface): Promise<void> => {
 		try {
-			await createApiCountry(value);
+			await handleFormAction<CountriesInterface>(
+				slug,
+				value,
+				createApiCountry,
+				updateApiCountry,
+			);
+
 			router.push('/admin/countries');
 		} catch (error: unknown) {
 			if (error instanceof Error) {
-				console.error('Error creating country:', error.message);
+				console.error('Error sending country form:', error.message);
 			} else {
-				console.error('An unknown error occurred during country creation.');
+				console.error('An unknown error occurred during country sending.');
 			}
 			throw error;
 		}
@@ -32,42 +59,41 @@ export const AdminCountriesForm: FC = () => {
 		<div className='form-holder'>
 			<Form
 				onSubmit={onSubmit}
-				render={({ handleSubmit, submitFailed }) => {
-					return (
-						<>
-							<FormLayout
-								className='row align-items-center'
-								onSubmit={handleSubmit}
-								title={t('base.countries')}
-							>
-								<InputText
-									className='col-md-4'
-									name='country'
-									placeholder={t('admin.country_name')}
-									required
-								/>
-								<InputText
-									className='col-md-4'
-									name='iso_code_2'
-									placeholder={t('admin.iso_code_2')}
-									validationHandler={isoCode2Validate}
-								/>
-								<InputText
-									className='col-md-4'
-									name='iso_code_3'
-									placeholder={t('admin.iso_code_3')}
-									validationHandler={isoCode3Validate}
-								/>
-								<InputCheckbox
-									name='postcode_required'
-									label={t('admin.postcode_required')}
-								/>
-								<InputCheckbox name='status' label={t('inputs.status')} />
-							</FormLayout>
-							<ErrorAlert submitFailed={submitFailed} />
-						</>
-					);
-				}}
+				initialValues={initialValues}
+				render={({ handleSubmit, submitFailed }) => (
+					<>
+						<FormLayout
+							className='row align-items-center'
+							onSubmit={handleSubmit}
+							title={t('base.countries')}
+						>
+							<InputText
+								className='col-md-4'
+								name='country'
+								placeholder={t('admin.country_name')}
+								required
+							/>
+							<InputText
+								className='col-md-4'
+								name='iso_code_2'
+								placeholder={t('admin.iso_code_2')}
+								validationHandler={isoCode2Validate}
+							/>
+							<InputText
+								className='col-md-4'
+								name='iso_code_3'
+								placeholder={t('admin.iso_code_3')}
+								validationHandler={isoCode3Validate}
+							/>
+							<InputCheckbox
+								name='postcode_required'
+								label={t('admin.postcode_required')}
+							/>
+							<InputCheckbox name='status' label={t('inputs.status')} />
+						</FormLayout>
+						<ErrorAlert submitFailed={submitFailed} />
+					</>
+				)}
 			/>
 		</div>
 	);
