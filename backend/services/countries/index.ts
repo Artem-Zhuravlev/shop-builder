9dfb457b4223e6723ee30e234a31a07e14f3dea1
Artem <mstrjesm@gmail.com>
2024-09-28 15:14:49 +0300

    feat(getCountry, updateCountry): add to public api

--- backend/services/countries/index.ts
+++ backend/services/countries/index.ts
@@ -1,2 +1,4 @@
 export { createCountry } from './createCountry';
 export { getCountries } from './getCountries';
+export { getCountry } from './getCountry';
+export { updateCountry } from './updateCountry';
