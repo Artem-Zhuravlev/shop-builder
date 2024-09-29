6728dc9180a4ae4126726889b6b8e79d7efc02f7
Artem <mstrjesm@gmail.com>
2024-09-29 14:15:06 +0300

    feat(deleteApiCountry): add to public api

--- src/components/shared/api/admin/countries/index.ts
+++ src/components/shared/api/admin/countries/index.ts
@@ -1,3 +1,4 @@
 export { createApiCountry } from './createApiCountry';
 export { getApiCountries } from './getApiCountries';
 export { updateApiCountry } from './updateApiCountry';
+export { deleteApiCountry } from './deleteApiCountry';
