70eecaa25e272a448701070c0f040ce8a9f04d16
Artem <mstrjesm@gmail.com>
2024-09-29 14:30:55 +0300

    feat(api/coutries): add DELETE endpoint

--- app/api/countries/route.ts
+++ app/api/countries/route.ts
@@ -1,6 +1,10 @@
 import { getPaginationParams } from '@/utils/getPaginationParams';
 import type { CountriesInterface } from '@interfaces/index';
-import { createCountry, getCountries } from 'backend/services/countries';
+import {
+	createCountry,
+	deleteCountry,
+	getCountries,
+} from 'backend/services/countries';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
@@ -32,6 +36,32 @@ export async function GET(request: NextRequest) {
 			error instanceof Error ? error.message : 'Unknown error occurred';
 		return NextResponse.json(
 			{ message: 'Error fetching countries', error: errorMessage },
+			{ status: 500 },
+		);
+	}
+}
+
+export async function DELETE(request: NextRequest) {
+	try {
+		const ids: number[] = await request.json();
+
+		if (!Array.isArray(ids) || ids.length === 0) {
+			return NextResponse.json(
+				{ message: 'Invalid input: Please provide an array of country IDs.' },
+				{ status: 400 },
+			);
+		}
+
+		const deletedCountries = await deleteCountry(ids);
+
+		return NextResponse.json(deletedCountries, { status: 200 });
+	} catch (error) {
+		console.error('Error deleting country:', error);
+		const errorMessage =
+			error instanceof Error ? error.message : 'Unknown error occurred';
+
+		return NextResponse.json(
+			{ message: 'Error deleting country', error: errorMessage },
 			{ status: 500 },
 		);
 	}
