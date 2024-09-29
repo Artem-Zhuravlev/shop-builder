8b2fa5ec1fca6961ef49f06e99f2bac7cf08bb83
Artem <mstrjesm@gmail.com>
2024-09-29 14:23:00 +0300

    feat(deleteCountry): add to public api

--- backend/services/countries/index.ts
+++ backend/services/countries/index.ts
@@ -2,3 +2,4 @@ export { createCountry } from './createCountry';
 export { getCountries } from './getCountries';
 export { getCountry } from './getCountry';
 export { updateCountry } from './updateCountry';
+export { deleteCountry } from './deleteCountry';
