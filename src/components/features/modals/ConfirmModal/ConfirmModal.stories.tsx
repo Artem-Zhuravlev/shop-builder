2dbd0e465746af21c4dd7dee01c16332529c2eda
Artem <mstrjesm@gmail.com>
2024-10-26 15:37:16 +0300

    feat(ConfirmModal): story with confirm-modal provider

--- src/components/features/modals/ConfirmModal/ConfirmModal.stories.tsx
+++ src/components/features/modals/ConfirmModal/ConfirmModal.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import { ConfirmModal } from './ConfirmModal';
+import { useConfirmModal } from '@/lib/modalProvider/ModalContext';
 
 export default {
 	title: 'Features/Modals/ConfirmModal',
@@ -7,10 +8,16 @@ export default {
 } as Meta<typeof ConfirmModal>;
 
 const Template: StoryFn<typeof ConfirmModal> = (args) => {
-	return <ConfirmModal {...args} />;
+	const { openModal } = useConfirmModal();
+
+	return (
+		<>
+			<button type='button' onClick={openModal}>
+				Open Modal
+			</button>
+		</>
+	);
 };
 
 export const Default = Template.bind({});
-Default.args = {
-	title: 'Main image',
-};
+Default.args = {};
