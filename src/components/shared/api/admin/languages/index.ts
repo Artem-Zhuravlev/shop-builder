2573440499a572ba322d5045bdb6f7d4600b3b0a
Artem <mstrjesm@gmail.com>
2024-10-30 20:13:24 +0200

    feat: add languages methods to public api

---
+++ src/components/shared/api/admin/languages/index.ts
@@ -0,0 +1,5 @@
+export { createApiLanguage } from './createApiLanguage';
+export { getApiLanguages } from './getApiLanguages';
+export { getApiLanguage } from './getApiLanguage';
+export { updateApiLanguage } from './updateApiLanguage';
+export { deleteApiLanguage } from './deleteApiLanguage';
