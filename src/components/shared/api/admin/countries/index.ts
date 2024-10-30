a03c0fbeb452c1f83d4d44fd0ed3b0ff3b7728ff
Artem <mstrjesm@gmail.com>
2024-10-30 21:28:42 +0200

    feat: add getApiCountry to public api

--- src/components/shared/api/admin/countries/index.ts
+++ src/components/shared/api/admin/countries/index.ts
@@ -1,4 +1,5 @@
 export { createApiCountry } from './createApiCountry';
 export { getApiCountries } from './getApiCountries';
+export { getApiCountry } from './getApiCountry';
 export { updateApiCountry } from './updateApiCountry';
 export { deleteApiCountry } from './deleteApiCountry';
