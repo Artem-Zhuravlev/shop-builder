4b65f96a13a0d2e5213fba714ec3ced790475d74
Artem <mstrjesm@gmail.com>
2024-10-30 17:38:25 +0200

    refactor(getCountries): add filtering by params

--- backend/services/countries/getCountries.ts
+++ backend/services/countries/getCountries.ts
@@ -1,12 +1,31 @@
 import { Countries } from './../../entities/Countries';
 import type { PaginateInterface } from '@interfaces/index';
 import { paginateService } from '../paginateService';
+import { type FindOptionsWhere, ILike } from 'typeorm';
 
 export const getCountries = async (
 	offset: number,
 	limit: number,
+	countryName?: string,
+	isoCode2?: string,
+	isoCode3?: string,
 ): Promise<PaginateInterface<Countries>> => {
+	const filters: FindOptionsWhere<Countries> = {};
+
+	if (countryName) {
+		filters.country = ILike(`%${countryName}%`);
+	}
+
+	if (isoCode2) {
+		filters.iso_code_2 = isoCode2;
+	}
+
+	if (isoCode3) {
+		filters.iso_code_3 = isoCode3;
+	}
+
 	return await paginateService(Countries, offset, limit, {
+		where: filters,
 		order: {
 			country: 'ASC',
 		},
