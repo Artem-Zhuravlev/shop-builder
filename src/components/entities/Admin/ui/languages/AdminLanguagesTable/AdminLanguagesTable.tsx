ba85e40cac80cc7bbf194c729e8701c49d76dc21
Artem <mstrjesm@gmail.com>
2024-10-30 21:30:32 +0200

    refactor(AdminLanguagesTable): add onDelete prop

--- src/components/entities/Admin/ui/languages/AdminLanguagesTable/AdminLanguagesTable.tsx
+++ src/components/entities/Admin/ui/languages/AdminLanguagesTable/AdminLanguagesTable.tsx
@@ -28,24 +28,25 @@ import {
 import { useTheme } from '@table-library/react-table-library/theme';
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
-import React, { type FC } from 'react';
+import React, { type FC, useState } from 'react';
+import { AdminTableNavbar } from '../../AdminTableNavbar/AdminTableNavbar';
+import type {
+	Action,
+	State,
+} from '@table-library/react-table-library/types/common';
 
-interface AdminLanguagesTableItem extends TableNode {
-	id: string | number;
-	language_name: string;
-	code: string;
-	sort_order: number;
-	slug: string;
-}
+interface LanguagesInterface extends TableNode {}
 
 interface AdminLanguagesTableProps {
-	nodes: AdminLanguagesTableItem[];
+	nodes: LanguagesInterface[];
+	onDelete: (ids: number[]) => void;
 }
 
 export const AdminLanguagesTable: FC<AdminLanguagesTableProps> = (props) => {
-	const { nodes } = props;
+	const { nodes, onDelete } = props;
 	const data = { nodes };
 	const t = useTranslations('admin');
+	const [selectedRows, setSelectedRows] = useState([]);
 	const router = useRouter();
 	const theme = useTheme({
 		...getTheme(),
@@ -53,6 +54,14 @@ export const AdminLanguagesTable: FC<AdminLanguagesTableProps> = (props) => {
 			'--data-table-library_grid-template-columns:  50px 2fr 1fr 1fr 150px;',
 		...defaultTableStyles,
 	});
+
+	const onSelectChange = (action: Action, state: State) => {
+		setSelectedRows(state.ids);
+	};
+
+	const onSortChange = (action: any, state: any) => {
+		console.log(action, state);
+	};
 
 	const sort = useSort(
 		data,
@@ -76,60 +85,60 @@ export const AdminLanguagesTable: FC<AdminLanguagesTableProps> = (props) => {
 		onChange: onSelectChange,
 	});
 
-	function onSortChange(action: any, state: any) {
-		console.log(action, state);
-	}
-
-	function onSelectChange(action: any, state: any) {
-		console.log(action, state);
-	}
-
 	return (
-		<Table
-			data={data}
-			theme={theme}
-			layout={{ custom: true }}
-			sort={sort}
-			select={select}
-		>
-			{(tableList: AdminLanguagesTableItem[]) => (
-				<>
-					<Header>
-						<HeaderRow>
-							<HeaderCellSelect />
-							<HeaderCellSort sortKey='LANGUAGE_NAME'>
-								{t('language_name')}
-							</HeaderCellSort>
-							<HeaderCellSort sortKey='CODE'>{t('code')}</HeaderCellSort>
-							<HeaderCellSort sortKey='SORT_ORDER'>
-								{t('sort_order')}
-							</HeaderCellSort>
-							<HeaderCell>{t('action')}</HeaderCell>
-						</HeaderRow>
-					</Header>
+		<>
+			<AdminTableNavbar
+				title={t('languages')}
+				route='/admin/languages'
+				onDelete={() => onDelete(selectedRows)}
+				isDeleteBtn={selectedRows.length > 0}
+			/>
+			<Table
+				data={data}
+				theme={theme}
+				layout={{ custom: true }}
+				sort={sort}
+				select={select}
+			>
+				{(tableList: LanguagesInterface[]) => (
+					<>
+						<Header>
+							<HeaderRow>
+								<HeaderCellSelect />
+								<HeaderCellSort sortKey='LANGUAGE_NAME'>
+									{t('language_name')}
+								</HeaderCellSort>
+								<HeaderCellSort sortKey='CODE'>{t('code')}</HeaderCellSort>
+								<HeaderCellSort sortKey='SORT_ORDER'>
+									{t('sort_order')}
+								</HeaderCellSort>
+								<HeaderCell>{t('action')}</HeaderCell>
+							</HeaderRow>
+						</Header>
 
-					<Body>
-						{tableList.map((item) => (
-							<Row item={item} key={item.id}>
-								<CellSelect item={item} />
-								<Cell>{item.language_name}</Cell>
-								<Cell>{item.code}</Cell>
-								<Cell>{item.sort_order}</Cell>
-								<Cell>
-									<ButtonBase
-										onClick={() =>
-											router.push(`${ADMIN_ROUTES.languages}/${item.slug}`)
-										}
-									>
-										<span className='icon-pencil' />
-									</ButtonBase>
-								</Cell>
-							</Row>
-						))}
-					</Body>
-				</>
-			)}
-		</Table>
+						<Body>
+							{tableList.map((item) => (
+								<Row item={item} key={item.id}>
+									<CellSelect item={item} />
+									<Cell>{item.name}</Cell>
+									<Cell>{item.code}</Cell>
+									<Cell>{item.sort_order}</Cell>
+									<Cell>
+										<ButtonBase
+											onClick={() =>
+												router.push(`${ADMIN_ROUTES.languages}/${item.id}`)
+											}
+										>
+											<span className='icon-pencil' />
+										</ButtonBase>
+									</Cell>
+								</Row>
+							))}
+						</Body>
+					</>
+				)}
+			</Table>
+		</>
 	);
 };
 
