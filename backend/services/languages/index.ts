ea49ce40510e4c39b79e6b2abb44918d652b86c8
Artem <mstrjesm@gmail.com>
2024-10-30 19:27:10 +0200

    feat(service/languages): add to public api

---
+++ backend/services/languages/index.ts
@@ -0,0 +1,5 @@
+export { createLanguage } from './createLanguage';
+export { deleteLanguage } from './deleteLanguage';
+export { getLanguage } from './getLanguage';
+export { getLanguages } from './getLanguages';
+export { updateLanguage } from './updateLanguage';
