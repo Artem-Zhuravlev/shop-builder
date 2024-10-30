23164b103bb1597a85c7645d40e8dd6198235770
Artem <mstrjesm@gmail.com>
2024-10-30 18:44:51 +0200

    feat(LanguagesInterface): add to public api

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { SettingsInterface } from './settings/settings.interface';
 export type { CountriesInterface } from './countries/countries.interface';
 export type { PaginateInterface } from './paginate/paginate.interface';
+export type { LanguagesInterface } from './languages/languages.interface';
