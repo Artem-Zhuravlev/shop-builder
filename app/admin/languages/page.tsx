c92acea03900a64ed7bb659e04a821e21a4b5165
Artem <mstrjesm@gmail.com>
2024-10-30 21:24:26 +0200

    feat(app/admin/languages): add AdminLanguagesLayout

--- app/admin/languages/page.tsx
+++ app/admin/languages/page.tsx
@@ -1,53 +1,8 @@
 import React from 'react';
-import { AdminTableLayout } from '@/components/widgets/AdminTableLayout';
-import { AdminLanguagesTable } from '@/components/entities/Admin';
-import { Paginate } from '@/components/shared/Paginate';
+import { AdminLanguagesLayout } from '@/components/entities/Admin/ui/languages';
 
 export default function LanguagesPage() {
-	const nodes = [
-		{
-			id: 1,
-			sort_order: 1,
-			language_name: 'English',
-			code: 'en-gb',
-			slug: 'english',
-		},
-		{
-			id: 2,
-			sort_order: 2,
-			language_name: 'Spanish',
-			code: 'es-es',
-			slug: 'spanish',
-		},
-		{
-			id: 3,
-			sort_order: 3,
-			language_name: 'French',
-			code: 'fr-fr',
-			slug: 'french',
-		},
-		{
-			id: 4,
-			sort_order: 4,
-			language_name: 'German',
-			code: 'de-de',
-			slug: 'german',
-		},
-		{
-			id: 5,
-			sort_order: 5,
-			language_name: 'Italian',
-			code: 'it-it',
-			slug: 'italian',
-		},
-	];
-
-	return (
-		<AdminTableLayout>
-			<AdminLanguagesTable nodes={nodes} />
-			<Paginate total={5} />
-		</AdminTableLayout>
-	);
+	return <AdminLanguagesLayout />;
 }
 
 export const metadata = {
